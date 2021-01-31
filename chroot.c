a4f434acb0100abe51c96e3c35f6eb53c2048d1c
Gaël PORTAY <gael.portay@collabora.com>
2021-01-31 06:52:34 -0500

    chroot: add debug traces

--- chroot.c
+++ chroot.c
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020 Gaël PORTAY
+ * Copyright 2020-2021 Gaël PORTAY
  *
  * SPDX-License-Identifier: LGPL-2.1
  */
@@ -19,13 +19,18 @@ int chroot(const char *path)
 	char buf[PATH_MAX];
 
 	if (getenv("IAMROOT_DEBUG"))
-		fprintf(stderr, "%s(path: '%s')\n", __func__, path);
+		fprintf(stderr, "%s(path: '%s') IAMROOT_ROOT='%s'\n", __func__,
+				path, getenv("IAMROOT_ROOT") ?: "");
 
 	real_path = path_resolution(path, buf, sizeof(buf));
 	if (!real_path) {
 		perror("path_resolution");
 		return -1;
 	}
+
+	if (getenv("IAMROOT_DEBUG"))
+		fprintf(stderr, "%s(path: '%s') IAMROOT_ROOT='%s'\n", __func__,
+				real_path, getenv("IAMROOT_ROOT") ?: "");
 
 	return setenv("IAMROOT_ROOT", real_path, 1);
 }

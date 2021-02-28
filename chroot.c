1bcfa5ca08cf6b30dec8e93338e556ef90bd561a
Gaël PORTAY <gael.portay@collabora.com>
2021-02-28 00:59:48 -0500

    chroot: adjust PATH and LD_LIBRARY_PATH

--- chroot.c
+++ chroot.c
@@ -14,6 +14,84 @@
 
 #include "path_resolution.h"
 
+static int prependenv(const char *root, const char *name, const char *value,
+		      int overwrite)
+{
+	char *old_value, *new_value = NULL, *tmp = NULL;
+	char *token, *saveptr;
+	size_t len = 0;
+	int ret = -1;
+	char *str;
+
+	old_value = getenv(name);
+	if (old_value && *old_value)
+		len += strlen(old_value);
+
+	tmp = strdup(value);
+	if (!tmp)
+		goto exit;
+
+	len += strlen(tmp);
+
+	token = strtok_r(tmp, ":", &saveptr);
+	if (token && *token) {
+		len += strlen(root);
+		if (old_value && *old_value)
+			len++;
+		while ((token = strtok_r(NULL, ":", &saveptr)))
+			len += strlen(root);
+	}
+
+	if (!len) {
+		ret = 0;
+		goto exit;
+	}
+
+	len++; /* NUL */
+	new_value = malloc(len);
+	if (!new_value)
+		goto exit;
+
+	*new_value = 0;
+	str = new_value;
+
+	strcpy(tmp, value);
+	token = strtok_r(tmp, ":", &saveptr);
+	if (token && *token) {
+		int n;
+
+		n = snprintf(str, len, "%s%s", root, token);
+		str += n;
+		len -= n;
+		while ((token = strtok_r(NULL, ":", &saveptr))) {
+			n = snprintf(str, len, ":%s%s", root, token);
+			str += n;
+			len -= n;
+		}
+	}
+
+	if (old_value && *old_value) {
+		int n;
+
+		if (old_value && *old_value) {
+		   *str++ = ':';
+		   len--;
+		}
+
+		n = snprintf(str, len, "%s", old_value);
+		str += n;
+		len -= n;
+	}
+
+	ret = setenv(name, new_value, overwrite);
+
+exit:
+	free(tmp);
+	free(new_value);
+
+	return ret;
+}
+
 int chroot(const char *path)
 {
 	const char *real_path;
@@ -45,6 +123,9 @@ int chroot(const char *path)
 			return -1;
 		}
 	}
+
+	setenv("PATH", "/bin:/usr/bin", 1);
+	prependenv(real_path, "LD_LIBRARY_PATH", "/usr/lib:/lib", 1);
 
 	if (getenv("IAMROOT_DEBUG"))
 		fprintf(stderr, "%s(path: '%s') IAMROOT_ROOT='%s'\n", __func__,

00ac80be9a7d1040b8003b0768239055ff95ecad
Gaël PORTAY <gael.portay@collabora.com>
2021-03-08 04:43:29 -0500

    tests: add chown and lchown

---
+++ tests/chown.c
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2021 Gaël PORTAY
+ *
+ * SPDX-License-Identifier: LGPL-2.1
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+#include <unistd.h>
+
+int main(int argc, char * const argv[])
+{
+	uid_t owner;
+	gid_t group;
+	int fd;
+
+	if (argc < 4) {
+		fprintf(stderr, "Too few arguments\n");
+		exit(EXIT_FAILURE);
+	} else if (argc > 4) {
+		fprintf(stderr, "Too many arguments\n");
+		exit(EXIT_FAILURE);
+	}
+
+	owner = strtoul(argv[2], NULL, 0);
+	group = strtoul(argv[3], NULL, 0);
+
+	fd = open(argv[1], O_CREAT, 0644);
+	if (fd == -1) {
+		perror("open");
+		return EXIT_FAILURE;
+	}
+
+	if (close(fd))
+		perror("close");
+
+	if (chown(argv[1], owner, group)) {
+		perror("chown");
+		return EXIT_FAILURE;
+	}
+
+	return EXIT_SUCCESS;
+}
---
+++ tests/lchown.c
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2021 Gaël PORTAY
+ *
+ * SPDX-License-Identifier: LGPL-2.1
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#include <unistd.h>
+
+int main(int argc, char * const argv[])
+{
+	uid_t owner;
+	gid_t group;
+
+	if (argc < 4) {
+		fprintf(stderr, "Too few arguments\n");
+		exit(EXIT_FAILURE);
+	} else if (argc > 4) {
+		fprintf(stderr, "Too many arguments\n");
+		exit(EXIT_FAILURE);
+	}
+
+	owner = strtoul(argv[2], NULL, 0);
+	group = strtoul(argv[3], NULL, 0);
+
+	if (symlink("file", argv[1])) {
+		perror("symlink");
+		return EXIT_FAILURE;
+	}
+
+	if (lchown(argv[1], owner, group)) {
+		perror("lchown");
+		return EXIT_FAILURE;
+	}
+
+	return EXIT_SUCCESS;
+}

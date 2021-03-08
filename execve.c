f37becac53220ed0859ba2f051d40efac5045cba
Gaël PORTAY <gael.portay@collabora.com>
2021-03-08 16:53:58 -0500

    execve: handle hashbang interpreter
    execve() a file with an hashbang resolves the path twice:
     - first by execve()
     - then by open() (by the interpreter)
    
    It results in having the path resolved to $ROOT/$ROOT/path[*] and leads
    to ENOENT or such errors:
     - execve(path) resolves path to $ROOT/path
     - open($ROOT/path) resolves $ROOT/path to $ROOT/$ROOT/path
    
    This handles hashbang interpreter by doing a recursive call to execv()
    using the not-resolved interpreter in place of path and prepending it,
    its single all-in-one argument if any, and the path to argv. Therefore,
    the path is resolved only once by the next call to open() which opens
    $ROOT/path:
     - execve(path, argv) -> execve(interp, interp + path + argv)
     - open(path) -> $ROOT/path
    
    [*]: ie. $IAMROOT_ROOT

--- execve.c
+++ execve.c
@@ -11,6 +11,7 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <stdio.h>
+#include <ctype.h>
 #include <string.h>
 #include <errno.h>
 #include <limits.h>
@@ -23,6 +24,9 @@
 #include <unistd.h>
 
 #include "path_resolution.h"
+
+/* See https://www.in-ulm.de/~mascheck/various/shebang/#results */
+#define HASHBANG_MAX NAME_MAX
 
 extern int __fprintf(FILE *, const char *, ...) __attribute__ ((format(printf,2,3)));
 extern int next_open(const char *, int, mode_t);
@@ -90,6 +94,52 @@ close:
 	return ret;
 }
 
+static ssize_t ishashbang(const char *path, char *buf, size_t bufsize)
+{
+	ssize_t siz;
+	char *d, *s;
+	int fd;
+
+	fd = next_open(path, O_RDONLY, 0);
+	if (fd == -1)
+		return -1;
+
+	siz = read(fd, buf, bufsize-1);
+	if (siz == -1)
+		goto close;
+
+	buf[siz] = 0;
+	if ((siz < 2) || (buf[0] != '#') || (buf[1] != '!')) {
+		siz = 0;
+		goto close;
+	}
+
+	s = buf+2;
+	d = buf;
+
+	while (isblank(*s))
+		s++;
+	while (*s && *s != '\n' && !isblank(*s))
+		*d++ = *s++;
+	*d++ = 0;
+
+	if (isblank(*s)) {
+		while (isblank(*s))
+			s++;
+		while (*s && *s != '\n' && !isblank(*s))
+			*d++ = *s++;
+		*d++ = 0;
+	}
+
+	siz = d-buf-1;
+
+close:
+	if (close(fd))
+		perror("close");
+
+	return siz;
+}
+
 static inline int canpreload(const char *path)
 {
 	int ret = -1;
@@ -125,7 +175,9 @@ int next_execve(const char *path, char * const argv[], char * const envp[])
 int execve(const char *path, char *const argv[], char * const envp[])
 {
 	const char *real_path;
+	char interp[HASHBANG_MAX];
 	char buf[PATH_MAX];
+	ssize_t siz;
 
 	real_path = path_resolution(path, buf, sizeof(buf), 0);
 	if (!real_path) {
@@ -144,6 +196,50 @@ int execve(const char *path, char *const argv[], char * const envp[])
 		return -1;
 	}
 
+	if (strcmp(path, real_path) == 0)
+		goto next;
+
+	siz = ishashbang(real_path, interp, sizeof(interp));
+	if (siz == -1) {
+		return -1;
+	} else if (siz > 0) {
+		char * const *arg;
+		char *interparg;
+		size_t len;
+		int argc;
+
+		argc = 1;
+		arg = argv;
+		while (*arg++)
+			argc++;
+
+		len = strlen(interp);
+		interparg = (len < (size_t)siz) ? &interp[len+1] : NULL;
+		if (interparg)
+			argc++;
+
+		if (argc < ARG_MAX) {
+			char *nargv[argc + 1];
+			char **narg;
+
+			narg = nargv;
+			*narg++ = (char *)interp;
+			if (interparg)
+				*narg++ = (char *)interparg;
+
+			arg = argv;
+			while (*arg)
+				*narg++ = *arg++;
+			*narg++ = NULL;
+
+			return execve(interp, nargv, environ);
+		}
+
+		errno = EINVAL;
+		return -1;
+	}
+
+next:
 	__fprintf(stderr, "%s(path: '%s' -> '%s', argv: '%s'... , envp: @%p...)\n",
 			  __func__, path, real_path, argv[0], envp[0]);
 

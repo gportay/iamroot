4d6b80edeef35b5bf8f680091ed4bc69ae07643b
Gaël PORTAY <gael.portay@collabora.com>
2021-04-06 05:53:52 -0400

    path_resolution: remove parameter names

--- path_resolution.c
+++ path_resolution.c
@@ -7,8 +7,7 @@
 #include <stddef.h>
 #include <fcntl.h>
 
-extern char *fpath_resolutionat(int fd, const char *path, char *buf,
-				size_t bufsize, int flags);
+extern char *fpath_resolutionat(int, const char *, char *, size_t, int);
 
 char *path_resolution(const char *path, char *buf, size_t bufsize, int flags)
 {

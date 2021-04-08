53a41be806231eaba7d89f40fc1c47a43d0dff76
Gaël PORTAY <gael.portay@collabora.com>
2021-04-08 13:27:40 -0400

    __fprintf: print pid if debug > 2

--- __fprintf.c
+++ __fprintf.c
@@ -4,6 +4,7 @@
  * SPDX-License-Identifier: LGPL-2.1
  */
 
+#include <unistd.h>
 #include <stdlib.h>
 #include <stdarg.h>
 
@@ -13,13 +14,18 @@
 
 int __vfprintf(FILE *f, const char *fmt, va_list ap)
 {
+	int ret = 0;
 	int debug;
 
 	debug = strtoul(getenv("IAMROOT_DEBUG") ?: "0", NULL, 0);
 	if (debug < 1 || !inchroot())
 		return 0;
 
-	return vfprintf(f, fmt, ap);
+	if (debug > 2)
+		ret += fprintf(stderr, "pid: %u: ", getpid());
+
+	ret += vfprintf(f, fmt, ap);
+	return ret;
 }
 
 int __fprintf(FILE *f, const char *fmt, ...)

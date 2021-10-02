gportay/iamroot

commit aba4fb4463b3ca154d96be5ae6da60b92f8a1fb7

Add __xmknod() and __xmknodat()

__xmknod.c
@@ -1,0 +1,61 @@
+/*
+ * Copyright 2021 Gaël PORTAY
+ *
+ * SPDX-License-Identifier: LGPL-2.1
+ */
+
+#define _GNU_SOURCE
+
+#include <unistd.h>
+#include <stdio.h>
+#include <errno.h>
+#include <limits.h>
+#include <dlfcn.h>
+#include <fcntl.h>
+
+#include <sys/stat.h>
+
+#include "path_resolution.h"
+
+extern int __fprintf(FILE *, const char *, ...) __attribute__ ((format(printf,2,3)));
+
+int next___xmknod(int ver, const char *path, mode_t mode, dev_t dev)
+{
+	int (*sym)(int, const char *, mode_t, dev_t);
+
+	sym = dlsym(RTLD_NEXT, "__xmknod");
+	if (!sym) {
+		errno = ENOTSUP;
+		return -1;
+	}
+
+	return sym(ver, path, mode, dev);
+}
+
+int __xmknod(int ver, const char *path, mode_t mode, dev_t dev)
+{
+	const char *real_path;
+	char buf[PATH_MAX];
+	int fd;
+	(void)ver;
+	(void)dev;
+
+	real_path = path_resolution(path, buf, sizeof(buf), 0);
+	if (!real_path) {
+		perror("path_resolution");
+		return -1;
+	}
+
+	__fprintf(stderr, "%s(path: '%s' -> '%s')\n", __func__, path,
+			real_path);
+
+	fd = creat(path, mode);
+	if (fd == -1)
+		return -1;
+
+	if (close(fd))
+		perror("close");
+
+	errno = 0;
+	return 0;
+}

__xmknodat.c
@@ -1,0 +1,60 @@
+/*
+ * Copyright 2021 Gaël PORTAY
+ *
+ * SPDX-License-Identifier: LGPL-2.1
+ */
+
+#define _GNU_SOURCE
+
+#include <unistd.h>
+#include <stdio.h>
+#include <errno.h>
+#include <limits.h>
+#include <dlfcn.h>
+
+#include <fcntl.h>
+#include <sys/stat.h>
+
+#include "fpath_resolutionat.h"
+
+extern int __fprintf(FILE *, const char *, ...) __attribute__ ((format(printf,2,3)));
+
+int next___xmknodat(int ver, int fd, const char *path, mode_t mode, dev_t dev)
+{
+	int (*sym)(int, int, const char *, mode_t, dev_t);
+
+	sym = dlsym(RTLD_NEXT, "__xmknodat");
+	if (!sym) {
+		errno = ENOTSUP;
+		return -1;
+	}
+
+	return sym(ver, fd, path, mode, dev);
+}
+
+int __xmknodat(int ver, int fd, const char *path, mode_t mode, dev_t dev)
+{
+	const char *real_path;
+	char buf[PATH_MAX];
+	(void)ver;
+	(void)dev;
+
+	real_path = fpath_resolutionat(fd, path, buf, sizeof(buf), 0);
+	if (!real_path) {
+		perror("fpath_resolutionat");
+		return -1;
+	}
+
+	__fprintf(stderr, "%s(fd %i, path: '%s' -> '%s')\n", __func__, fd, path,
+			real_path);
+
+	fd = creat(real_path, mode);
+	if (fd == -1)
+		return -1;
+
+	if (close(fd))
+		perror("close");
+
+	errno = 0;
+	return 0;
+}

Makefile
@@ -12,6 +12,8 @@ all: libiamroot.so
 
 libiamroot.so: __fxstatat.o
 libiamroot.so: __lxstat.o
+libiamroot.so: __xmknod.o
+libiamroot.so: __xmknodat.o
 libiamroot.so: __xstat.o
 libiamroot.so: __fprintf.o
 libiamroot.so: __printf.o

commit 47916d71e6096db1149faa24fd25bf6af159efef

__xmknod{,at}: fix prototype
The glibc functions ____xmknod() and __xmknodat() use pointers for the
dev argument; the prototype were been wrong since the functions were
introduced with the commit aba4fb4463b3ca154d96be5ae6da60b92f8a1fb7.

__xmknod.c
@@ -14,9 +14,9 @@
 #include "iamroot.h"
 
 __attribute__((visibility("hidden")))
-int next___xmknod(int ver, const char *path, mode_t mode, dev_t dev)
+int next___xmknod(int ver, const char *path, mode_t mode, dev_t *dev)
 {
-	int (*sym)(int, const char *, mode_t, dev_t);
+	int (*sym)(int, const char *, mode_t, dev_t *);
 
 	sym = dlsym(RTLD_NEXT, "__xmknod");
 	if (!sym) {
@@ -28,7 +28,7 @@ int next___xmknod(int ver, const char *path, mode_t mode, dev_t dev)
 	return sym(ver, path, mode, dev);
 }
 
-int __xmknod(int ver, const char *path, mode_t mode, dev_t dev)
+int __xmknod(int ver, const char *path, mode_t mode, dev_t *dev)
 {
 	char buf[PATH_MAX];
 	char *real_path;

__xmknodat.c
@@ -15,9 +15,9 @@
 #include "iamroot.h"
 
 __attribute__((visibility("hidden")))
-int next___xmknodat(int ver, int fd, const char *path, mode_t mode, dev_t dev)
+int next___xmknodat(int ver, int fd, const char *path, mode_t mode, dev_t *dev)
 {
-	int (*sym)(int, int, const char *, mode_t, dev_t);
+	int (*sym)(int, int, const char *, mode_t, dev_t *);
 
 	sym = dlsym(RTLD_NEXT, "__xmknodat");
 	if (!sym) {
@@ -29,7 +29,7 @@ int next___xmknodat(int ver, int fd, const char *path, mode_t mode, dev_t dev)
 	return sym(ver, fd, path, mode, dev);
 }
 
-int __xmknodat(int ver, int fd, const char *path, mode_t mode, dev_t dev)
+int __xmknodat(int ver, int fd, const char *path, mode_t mode, dev_t *dev)
 {
 	char buf[PATH_MAX];
 	char *real_path;
92b29444d897a1fd68eeba9edb4b2f24a35bda79
Gaël PORTAY <gael.portay@collabora.com>
2021-10-02 16:55:41 +0200

    tests: add get_current_dir_name

---
+++ tests/get_current_dir_name.c
@@ -0,0 +1,23 @@
+/*
+ * Copyright 2021 Gaël PORTAY
+ *
+ * SPDX-License-Identifier: LGPL-2.1
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+#include <unistd.h>
+
+#include "iamroot.h"
+
+int main()
+{
+	char *cwd;
+
+	cwd = get_current_dir_name();
+	printf("%s\n", cwd);
+	free(cwd);
+
+	return EXIT_SUCCESS;
+}

gportay/iamroot

commit 2fedd900742d496317d84ff888ccad1f79252484

Add glibc 64 variants
Fixes:

	installing systemd...
	Failed to take /etc/passwd lock: Permission denied
	(...)
	( 3/10) Reloading system manager configuration...
	Failed to reload daemon: Transport endpoint is not connected
	error: command failed to execute correctly
	(...)
	( 6/10) Creating temporary files...
	Failed to copy files to /etc/crypttab: Permission denied
	Failed to copy files to /etc/gshadow: Permission denied
	Failed to copy files to /etc/shadow: Permission denied
	symlink(/etc/machine-id, /var/lib/dbus/machine-id) failed: No such file or directory
	Failed to open directory 'portables': Permission denied
	Failed to open directory 'machines': Permission denied
	Failed to open directory 'private': Permission denied
	Failed to open directory 'private': Permission denied
	Failed to open directory 'private': Permission denied
	Setting access ACL "u::rwx,g::r-x,g:wheel:r-x,g:adm:r-x,m::r-x,o::r-x" on /var/log/journal failed: Operation not permitted
	Failed to re-open '/var/log/journal': Operation not permitted
	Setting access ACL "u::rwx,g::r-x,g:wheel:r-x,g:adm:r-x,m::r-x,o::r-x" on /var/log/journal/403c047caf5e41c99814685d09c6bd55 failed: Operation not permitted
	Failed to re-open '/var/log/journal/403c047caf5e41c99814685d09c6bd55': Operation not permitted
	Failed to re-open '/var/log/journal/remote': Operation not permitted
	Setting access ACL "u::rwx,g::r-x,g:wheel:r-x,g:adm:r-x,m::r-x,o::r-x" on /run/log/journal failed: Operation not permitted
	Setting access ACL "u::rw-,g::r-x,g:wheel:r--,g:adm:r--,m::r--,o::---" on /var/log/journal/403c047caf5e41c99814685d09c6bd55/system.journal failed: Operation not permitted
	error: command failed to execute correctly
	(...)
	( 9/10) Rebuilding certificate stores...
	p11-kit: couldn't create file: /etc/ca-certificates/extracted/tls-ca-bundle.pem: Unknown error 13
	p11-kit: couldn't create file: /etc/ca-certificates/extracted/email-ca-bundle.pem: Unknown error 13

fstatat64.c
@@ -1,0 +1,56 @@
+/*
+ * Copyright 2021 Gaël PORTAY
+ *
+ * SPDX-License-Identifier: LGPL-2.1
+ */
+
+#define _GNU_SOURCE
+
+#include <stdio.h>
+#include <errno.h>
+#include <limits.h>
+#include <dlfcn.h>
+
+#include <fcntl.h>
+#include <sys/stat.h>
+
+#include "fpath_resolutionat.h"
+
+#ifdef __GLIBC__
+extern int __fprintf(FILE *, const char *, ...) __attribute__ ((format(printf,2,3)));
+extern int frootstatat64(int, const char *, struct stat64 *, int);
+
+__attribute__((visibility("hidden")))
+int next_fstatat64(int fd, const char *path, struct stat *statbuf, int flags)
+{
+	int (*sym)(int, const char *, struct stat *, int);
+
+	sym = dlsym(RTLD_NEXT, "fstatat64");
+	if (!sym) {
+		errno = ENOTSUP;
+		return -1;
+	}
+
+	return sym(fd, path, statbuf, flags);
+}
+
+int fstatat64(int fd, const char *path, struct stat64 *statbuf, int flags)
+{
+	char buf[PATH_MAX];
+	char *real_path;
+	int ret;
+
+	real_path = fpath_resolutionat(fd, path, buf, sizeof(buf), flags);
+	if (!real_path) {
+		perror("fpath_resolutionat");
+		return -1;
+	}
+
+	ret = frootstatat64(fd, real_path, statbuf, flags);
+
+	__fprintf(stderr, "%s(fd: %i, path: '%s' -> '%s', ...)\n", __func__, fd,
+			  path, real_path);
+
+	return ret;
+}
+#endif

Makefile
@@ -21,11 +21,15 @@ QEMU += -serial mon:stdio
 all: libiamroot.so
 
 libiamroot.so: __fxstatat.o
+libiamroot.so: __fxstatat64.o
 libiamroot.so: __lxstat.o
+libiamroot.so: __lxstat64.o
 libiamroot.so: __statfs.o
+libiamroot.so: __statfs64.o
 libiamroot.so: __xmknod.o
 libiamroot.so: __xmknodat.o
 libiamroot.so: __xstat.o
+libiamroot.so: __xstat64.o
 libiamroot.so: __fprintf.o
 libiamroot.so: __printf.o
 libiamroot.so: access.o
@@ -35,6 +39,7 @@ libiamroot.so: chmod.o
 libiamroot.so: chown.o
 libiamroot.so: chroot.o
 libiamroot.so: creat.o
+libiamroot.so: creat64.o
 libiamroot.so: dlmopen.o
 libiamroot.so: dlopen.o
 libiamroot.so: eaccess.o
@@ -53,9 +58,12 @@ libiamroot.so: fchown.o
 libiamroot.so: fchownat.o
 libiamroot.so: fexecve.o
 libiamroot.so: fopen.o
+libiamroot.so: fopen64.o
 libiamroot.so: fpath_resolutionat.o
 libiamroot.so: freopen.o
+libiamroot.so: freopen64.o
 libiamroot.so: fstatat.o
+libiamroot.so: fstatat64.o
 libiamroot.so: futimesat.o
 libiamroot.so: get_current_dir_name.o
 libiamroot.so: getcwd.o
@@ -73,6 +81,7 @@ libiamroot.so: llistxattr.o
 libiamroot.so: lremovexattr.o
 libiamroot.so: lsetxattr.o
 libiamroot.so: lstat.o
+libiamroot.so: lstat64.o
 libiamroot.so: lutimes.o
 libiamroot.so: mkdir.o
 libiamroot.so: mkdirat.o
@@ -81,13 +90,20 @@ libiamroot.so: mkfifoat.o
 libiamroot.so: mknod.o
 libiamroot.so: mknodat.o
 libiamroot.so: mkostemp.o
+libiamroot.so: mkostemp64.o
 libiamroot.so: mkostemps.o
+libiamroot.so: mkostemps64.o
 libiamroot.so: mkstemp.o
+libiamroot.so: mkstemp64.o
 libiamroot.so: mkstemps.o
+libiamroot.so: mkstemps64.o
 libiamroot.so: mount.o
 libiamroot.so: open.o
+libiamroot.so: open64.o
 libiamroot.so: openat.o
+libiamroot.so: openat64.o
 libiamroot.so: opendir.o
+libiamroot.so: opendir64.o
 libiamroot.so: path_resolution.o
 libiamroot.so: readlink.o
 libiamroot.so: readlinkat.o
@@ -102,8 +118,11 @@ libiamroot.so: scandir.o
 libiamroot.so: scandirat.o
 libiamroot.so: setxattr.o
 libiamroot.so: stat.o
+libiamroot.so: stat64.o
 libiamroot.so: statfs.o
+libiamroot.so: statfs64.o
 libiamroot.so: statvfs.o
+libiamroot.so: statvfs64.o
 libiamroot.so: statx.o
 libiamroot.so: symlink.o
 libiamroot.so: symlinkat.o
@@ -258,20 +277,20 @@ alpine-minirootfs-3.13.0-x86_64.tar.gz:
 	wget http://dl-cdn.alpinelinux.org/alpine/v3.13/releases/x86_64/alpine-minirootfs-3.13.0-x86_64.tar.gz
 
 .PHONY: arch-rootfs
-arch-rootfs: | arch-rootfs/usr/bin/pacman
+arch-rootfs: | arch-rootfs/etc/machine-id
 
 arch-rootfs/boot/vmlinuz-linux: export LD_PRELOAD = $(CURDIR)/libiamroot.so
 arch-rootfs/boot/vmlinuz-linux: export IAMROOT_EXEC = $(CURDIR)/exec.sh
 arch-rootfs/boot/vmlinuz-linux: export EUID = 0
 arch-rootfs/boot/vmlinuz-linux: export IAMROOT_FORCE = 1
-arch-rootfs/boot/vmlinuz-linux: libiamroot.so | arch-rootfs/usr/bin/pacman
+arch-rootfs/boot/vmlinuz-linux: libiamroot.so | arch-rootfs/etc/machine-id
 	pacman -r arch-rootfs --noconfirm -S linux
 
-arch-rootfs/usr/bin/pacman: export LD_PRELOAD = $(CURDIR)/libiamroot.so
-arch-rootfs/usr/bin/pacman: export IAMROOT_EXEC = $(CURDIR)/exec.sh
-arch-rootfs/usr/bin/pacman: export EUID = 0
-arch-rootfs/usr/bin/pacman: export IAMROOT_FORCE = 1
-arch-rootfs/usr/bin/pacman: libiamroot.so
+arch-rootfs/etc/machine-id: export LD_PRELOAD = $(CURDIR)/libiamroot.so
+arch-rootfs/etc/machine-id: export IAMROOT_EXEC = $(CURDIR)/exec.sh
+arch-rootfs/etc/machine-id: export EUID = 0
+arch-rootfs/etc/machine-id: export IAMROOT_FORCE = 1
+arch-rootfs/etc/machine-id: libiamroot.so
 	mkdir -p arch-rootfs
 	pacstrap arch-rootfs
 

__fxstatat64.c
@@ -1,0 +1,56 @@
+/*
+ * Copyright 2021 Gaël PORTAY
+ *
+ * SPDX-License-Identifier: LGPL-2.1
+ */
+
+#define _GNU_SOURCE
+
+#include <stdio.h>
+#include <errno.h>
+#include <limits.h>
+#include <dlfcn.h>
+
+#include <fcntl.h>
+#include <sys/stat.h>
+
+#include "fpath_resolutionat.h"
+
+extern int __fprintf(FILE *, const char *, ...) __attribute__ ((format(printf,2,3)));
+extern int __fxrootstatat64(int, int, const char *, struct stat *, int);
+
+__attribute__((visibility("hidden")))
+int next___fxstatat64(int ver, int fd, const char *path, struct stat *statbuf,
+		    int flags)
+{
+	int (*sym)(int, int, const char *, struct stat *, int);
+
+	sym = dlsym(RTLD_NEXT, "__fxstatat64");
+	if (!sym) {
+		errno = ENOTSUP;
+		return -1;
+	}
+
+	return sym(ver, fd, path, statbuf, flags);
+}
+
+int __fxstatat64(int ver, int fd, const char *path, struct stat *statbuf,
+	       int flags)
+{
+	char buf[PATH_MAX];
+	char *real_path;
+	int ret;
+
+	real_path = fpath_resolutionat(fd, path, buf, sizeof(buf), flags);
+	if (!real_path) {
+		perror("fpath_resolutionat");
+		return -1;
+	}
+
+	ret = __fxrootstatat64(ver, fd, real_path, statbuf, flags);
+
+	__fprintf(stderr, "%s(fd: %i, path: '%s' -> '%s', ...)\n", __func__, fd,
+			  path, real_path);
+
+	return ret;
+}

__lxstat64.c
@@ -1,0 +1,54 @@
+/*
+ * Copyright 2021 Gaël PORTAY
+ *
+ * SPDX-License-Identifier: LGPL-2.1
+ */
+
+#define _GNU_SOURCE
+
+#include <stdio.h>
+#include <errno.h>
+#include <limits.h>
+#include <dlfcn.h>
+
+#include <fcntl.h>
+#include <sys/stat.h>
+
+#include "path_resolution.h"
+
+extern int __fprintf(FILE *, const char *, ...) __attribute__ ((format(printf,2,3)));
+extern int __rootlxstat64(int, const char *, struct stat64 *);
+
+__attribute__((visibility("hidden")))
+int next___lxstat64(int ver, const char *path, struct stat64 *stat64buf)
+{
+	int (*sym)(int, const char *, struct stat64 *);
+
+	sym = dlsym(RTLD_NEXT, "__lxstat64");
+	if (!sym) {
+		errno = ENOTSUP;
+		return -1;
+	}
+
+	return sym(ver, path, stat64buf);
+}
+
+int __lxstat64(int ver, const char *path, struct stat64 *stat64buf)
+{
+	char buf[PATH_MAX];
+	char *real_path;
+	int ret;
+
+	real_path = path_resolution(path, buf, sizeof(buf), 0);
+	if (!real_path) {
+		perror("path_resolution");
+		return -1;
+	}
+
+	ret = __rootlxstat64(ver, real_path, stat64buf);
+
+	__fprintf(stderr, "%s(path: '%s' -> '%s', ...)\n", __func__, path,
+			  real_path);
+
+	return ret;
+}

__statfs64.c
@@ -1,0 +1,49 @@
+/*
+ * Copyright 2021 Gaël PORTAY
+ *
+ * SPDX-License-Identifier: LGPL-2.1
+ */
+
+#define _GNU_SOURCE
+
+#include <stdio.h>
+#include <errno.h>
+#include <limits.h>
+#include <dlfcn.h>
+
+#include <sys/statfs.h>
+
+#include "path_resolution.h"
+
+extern int __fprintf(FILE *, const char *, ...) __attribute__ ((format(printf,2,3)));
+
+__attribute__((visibility("hidden")))
+int next___statfs64(const char *path, struct statfs64 *statfs64buf)
+{
+	int (*sym)(const char *, struct statfs64 *);
+
+	sym = dlsym(RTLD_NEXT, "__statfs64");
+	if (!sym) {
+		errno = ENOTSUP;
+		return -1;
+	}
+
+	return sym(path, statfs64buf);
+}
+
+int __statfs64(const char *path, struct statfs64 *statfs64buf)
+{
+	char buf[PATH_MAX];
+	char *real_path;
+
+	real_path = path_resolution(path, buf, sizeof(buf), 0);
+	if (!real_path) {
+		perror("path_resolution");
+		return -1;
+	}
+
+	__fprintf(stderr, "%s(path: '%s' -> '%s', ...)\n", __func__, path,
+			  real_path);
+
+	return next___statfs64(real_path, statfs64buf);
+}

__xstat64.c
@@ -1,0 +1,54 @@
+/*
+ * Copyright 2021 Gaël PORTAY
+ *
+ * SPDX-License-Identifier: LGPL-2.1
+ */
+
+#define _GNU_SOURCE
+
+#include <stdio.h>
+#include <errno.h>
+#include <limits.h>
+#include <dlfcn.h>
+
+#include <fcntl.h>
+#include <sys/stat.h>
+
+#include "path_resolution.h"
+
+extern int __fprintf(FILE *, const char *, ...) __attribute__ ((format(printf,2,3)));
+extern int __rootxstat64(int, const char *, struct stat64 *);
+
+__attribute__((visibility("hidden")))
+int next___xstat64(int ver, const char *path, struct stat64 *stat64buf)
+{
+	int (*sym)(int, const char *, struct stat64 *);
+
+	sym = dlsym(RTLD_NEXT, "__xstat64");
+	if (!sym) {
+		errno = ENOTSUP;
+		return -1;
+	}
+
+	return sym(ver, path, stat64buf);
+}
+
+int __xstat64(int ver, const char *path, struct stat64 *stat64buf)
+{
+	char buf[PATH_MAX];
+	char *real_path;
+	int ret;
+
+	real_path = path_resolution(path, buf, sizeof(buf), 0);
+	if (!real_path) {
+		perror("path_resolution");
+		return -1;
+	}
+
+	ret = __rootxstat64(ver, real_path, stat64buf);
+
+	__fprintf(stderr, "%s(path: '%s' -> '%s', ...)\n", __func__, path,
+			  real_path);
+
+	return ret;
+}

chroot.c
@@ -25,9 +25,15 @@ extern char *next_getcwd(char *, size_t);
 extern int next_stat(const char *, struct stat *);
 extern int next_lstat(const char *, struct stat *);
 extern int next_fstatat(int, const char *, struct stat *, int);
+extern int next_stat64(const char *, struct stat64 *);
+extern int next_lstat64(const char *, struct stat64 *);
+extern int next_fstatat64(int, const char *, struct stat64 *, int);
 extern int next___xstat(int, const char *, struct stat *);
+extern int next___xstat64(int, const char *, struct stat64 *);
 extern int next___lxstat(int, const char *, struct stat *);
+extern int next___lxstat64(int, const char *, struct stat64 *);
 extern int next___fxstatat(int, int, const char *, struct stat *, int);
+extern int next___fxstatat64(int, int, const char *, struct stat64 *, int);
 #ifdef __GLIBC__
 extern int next_statx(int, const char *, int, unsigned int, struct statx *);
 #endif
@@ -226,6 +232,76 @@ exit:
 	return ret;
 }
 
+#ifdef __GLIBC__
+__attribute__((visibility("hidden")))
+int rootstat64(const char *path, struct stat64 *buf)
+{
+	uid_t uid;
+	gid_t gid;
+	int ret;
+
+	ret = next_stat64(path, buf);
+	if (ret == -1)
+		goto exit;
+
+	uid = next_geteuid();
+	if (buf->st_uid == uid)
+		buf->st_uid = geteuid();
+
+	gid = getegid();
+	if (buf->st_gid == gid)
+		buf->st_gid = 0;
+
+exit:
+	return ret;
+}
+
+__attribute__((visibility("hidden")))
+int lrootstat64(const char *path, struct stat64 *buf)
+{
+	uid_t uid;
+	gid_t gid;
+	int ret;
+
+	ret = next_lstat64(path, buf);
+	if (ret == -1)
+		goto exit;
+
+	uid = next_geteuid();
+	if (buf->st_uid == uid)
+		buf->st_uid = geteuid();
+
+	gid = getegid();
+	if (buf->st_gid == gid)
+		buf->st_gid = 0;
+
+exit:
+	return ret;
+}
+
+__attribute__((visibility("hidden")))
+int frootstatat64(int fd, const char *path, struct stat64 *buf, int flags)
+{
+	uid_t uid;
+	gid_t gid;
+	int ret;
+
+	ret = next_fstatat64(fd, path, buf, flags);
+	if (ret == -1)
+		goto exit;
+
+	uid = next_geteuid();
+	if (buf->st_uid == uid)
+		buf->st_uid = geteuid();
+
+	gid = getegid();
+	if (buf->st_gid == gid)
+		buf->st_gid = 0;
+
+exit:
+	return ret;
+}
+
 __attribute__((visibility("hidden")))
 int __rootxstat(int ver, const char *path, struct stat *buf)
 {
@@ -234,6 +310,29 @@ int __rootxstat(int ver, const char *path, struct stat *buf)
 	int ret;
 
 	ret = next___xstat(ver, path, buf);
+	if (ret == -1)
+		goto exit;
+
+	uid = next_geteuid();
+	if (buf->st_uid == uid)
+		buf->st_uid = geteuid();
+
+	gid = getegid();
+	if (buf->st_gid == gid)
+		buf->st_gid = 0;
+
+exit:
+	return ret;
+}
+
+__attribute__((visibility("hidden")))
+int __rootxstat64(int ver, const char *path, struct stat64 *buf)
+{
+	uid_t uid;
+	gid_t gid;
+	int ret;
+
+	ret = next___xstat64(ver, path, buf);
 	if (ret == -1)
 		goto exit;
 
@@ -273,6 +372,29 @@ exit:
 }
 
 __attribute__((visibility("hidden")))
+int __rootlxstat64(int ver, const char *path, struct stat64 *buf)
+{
+	uid_t uid;
+	gid_t gid;
+	int ret;
+
+	ret = next___lxstat64(ver, path, buf);
+	if (ret == -1)
+		goto exit;
+
+	uid = next_geteuid();
+	if (buf->st_uid == uid)
+		buf->st_uid = geteuid();
+
+	gid = getegid();
+	if (buf->st_gid == gid)
+		buf->st_gid = 0;
+
+exit:
+	return ret;
+}
+
+__attribute__((visibility("hidden")))
 int __fxrootstatat(int ver, int fd, const char *path, struct stat *buf,
 		   int flags)
 {
@@ -296,7 +418,30 @@ exit:
 	return ret;
 }
 
-#ifdef __GLIBC__
+__attribute__((visibility("hidden")))
+int __fxrootstatat64(int ver, int fd, const char *path, struct stat64 *buf,
+		     int flags)
+{
+	uid_t uid;
+	gid_t gid;
+	int ret;
+
+	ret = next___fxstatat64(ver, fd, path, buf, flags);
+	if (ret == -1)
+		goto exit;
+
+	uid = next_geteuid();
+	if (buf->st_uid == uid)
+		buf->st_uid = geteuid();
+
+	gid = getegid();
+	if (buf->st_gid == gid)
+		buf->st_gid = 0;
+
+exit:
+	return ret;
+}
+
 __attribute__((visibility("hidden")))
 int rootstatx(int fd, const char *path, int flags, unsigned int mask,
 	      struct statx *buf)
@@ -324,8 +469,8 @@ exit:
 
 int chroot(const char *path)
 {
+	const char *real_path;
 	char buf[PATH_MAX];
-	char *real_path;
 
 	/* prepend the current working directory for relative paths */
 	if (path[0] != '/') {

creat64.c
@@ -1,0 +1,52 @@
+/*
+ * Copyright 2021 Gaël PORTAY
+ *
+ * SPDX-License-Identifier: LGPL-2.1
+ */
+
+#define _GNU_SOURCE
+
+#include <stdio.h>
+#include <errno.h>
+#include <limits.h>
+#include <dlfcn.h>
+
+#include <sys/stat.h>
+#include <fcntl.h>
+
+#include "path_resolution.h"
+
+#ifdef __GLIBC__
+extern int __fprintf(FILE *, const char *, ...) __attribute__ ((format(printf,2,3)));
+
+__attribute__((visibility("hidden")))
+int next_creat64(const char *path, mode_t mode)
+{
+	int (*sym)(const char *, mode_t);
+
+	sym = dlsym(RTLD_NEXT, "creat64");
+	if (!sym) {
+		errno = ENOTSUP;
+		return -1;
+	}
+
+	return sym(path, mode);
+}
+
+int creat64(const char *path, mode_t mode)
+{
+	char buf[PATH_MAX];
+	char *real_path;
+
+	real_path = path_resolution(path, buf, sizeof(buf), 0);
+	if (!real_path) {
+		perror("path_resolution");
+		return -1;
+	}
+
+	__fprintf(stderr, "%s(path: '%s' -> '%s')\n", __func__, path,
+			real_path);
+
+	return next_creat64(path, mode);
+}
+#endif

commit 35853a531d967f4fbd03d04781f7aa1396a82ed3

fxstatat64: fix prototype
The glibc function fxstatat64() uses a stat64 structure; the prototype
for next_fstatat64() is wrong since the function was introduced with the
commit 2fedd900742d496317d84ff888ccad1f79252484.

However, the relying root function rootfstatat64() uses the right
proptype.

fstatat64.c
@@ -18,9 +18,9 @@
 extern int rootfstatat64(int, const char *, struct stat64 *, int);
 
 __attribute__((visibility("hidden")))
-int next_fstatat64(int fd, const char *path, struct stat *statbuf, int flags)
+int next_fstatat64(int fd, const char *path, struct stat64 *statbuf, int flags)
 {
-	int (*sym)(int, const char *, struct stat *, int);
+	int (*sym)(int, const char *, struct stat64 *, int);
 	int ret;
 
 	sym = dlsym(RTLD_NEXT, "fstatat64");
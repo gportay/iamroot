4c1c8a215c976aef1eeba99a6481ca9f640f918b
Gaël PORTAY <gael.portay@collabora.com>
2021-12-14 02:48:22 +0100

    tests: add getcwd and getwd

---
+++ tests/getwd.c
@@ -0,0 +1,24 @@
+/*
+ * Copyright 2021 Gaël PORTAY
+ *
+ * SPDX-License-Identifier: LGPL-2.1-or-later
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <limits.h>
+
+#include <unistd.h>
+
+#include "iamroot.h"
+
+int main()
+{
+	char buf[PATH_MAX];
+	char *cwd;
+
+	cwd = getwd(buf);
+	printf("%s\n", cwd);
+
+	return EXIT_SUCCESS;
+}

gportay/iamroot

commit 0b9374de24d5d1a5fd67e533c3760978cb7d319b

f*(): output the fd's path

fpath_resolutionat.c
@@ -13,7 +13,7 @@
 #include <dlfcn.h>
 #include <sys/stat.h>
 #include <regex.h>
-
+#include <assert.h>
 #include <fcntl.h>
 
 #include "iamroot.h"
@@ -21,7 +21,7 @@
 extern ssize_t next_readlink(const char *, char *, size_t);
 extern int next_lstat(const char *, struct stat *);
 
-static inline ssize_t __procfdreadlink(int fd, char *buf, size_t bufsize)
+ssize_t __procfdreadlink(int fd, char *buf, size_t bufsize)
 {
 	char tmp[sizeof("/proc/self/fd/") + 4];
 	__procfdname(tmp, fd);

__fperror.c
@@ -6,16 +6,29 @@
 
 #include <stdio.h>
 #include <errno.h>
+#include <limits.h>
 #include <signal.h>
 
 #include "iamroot.h"
 
 void __fperror(int fd, const char *s)
 {
+	char buf[PATH_MAX];
+	char *real_path;
+	ssize_t siz;
+
 	if ((errno != EPERM) && (errno != EACCES))
 		return;
 
-	__warning("%i: %s: %m\n", fd, s);
+	siz = __procfdreadlink(fd, buf, sizeof(buf));
+	if (siz == -1) {
+		perror("__procfdreadlink");
+		return;
+	}
+	buf[siz] = 0;
+	real_path = buf;
+
+	__warning("%i -> %s: %s: %m\n", fd, real_path, s);
 	if (__fatal())
 		raise(SIGABRT);
 }

__fstat64.c
@@ -38,7 +38,19 @@ int next___fstat64(int fd, struct stat64 *statbuf)
 
 int __fstat64(int fd, struct stat64 *statbuf)
 {
-	__verbose("%s(fd: %i, ...)\n", __func__, fd);
+	char buf[PATH_MAX];
+	char *real_path;
+	ssize_t siz;
+
+	siz = __procfdreadlink(fd, buf, sizeof(buf));
+	if (siz == -1) {
+		perror("__procfdreadlink");
+		return -1;
+	}
+	buf[siz] = 0;
+	real_path = buf;
+
+	__verbose("%s(fd: %i <-> '%s', ...)\n", __func__, fd, real_path);
 
 	return __rootfstat64(fd, statbuf);
 }

fchdir.c
@@ -6,6 +6,7 @@
 
 #include <stdio.h>
 #include <errno.h>
+#include <limits.h>
 #include <dlfcn.h>
 
 #include <unistd.h>
@@ -34,7 +35,18 @@ int next_fchdir(int fd)
 
 int fchdir(int fd)
 {
+	char buf[PATH_MAX];
+	char *real_path;
+	ssize_t siz;
 	int ret;
+
+	siz = __procfdreadlink(fd, buf, sizeof(buf));
+	if (siz == -1) {
+		perror("__procfdreadlink");
+		return -1;
+	}
+	buf[siz] = 0;
+	real_path = buf;
 
 	ret = next_fchdir(fd);
 	if (ret) {
@@ -42,7 +54,7 @@ int fchdir(int fd)
 		return ret;
 	}
 
-	__verbose("%s(fd: %i)\n", __func__, fd);
+	__verbose("%s(fd: %i -> '%s')\n", __func__, fd, real_path);
 
 	return chrootdir(NULL);
 }

fchmod.c
@@ -35,7 +35,20 @@ int next_fchmod(int fd, mode_t mode)
 
 int fchmod(int fd, mode_t mode)
 {
-	__verbose("%s(fd: %i, mode: 0%03o)\n", __func__, fd, mode);
+	char buf[PATH_MAX];
+	char *real_path;
+	ssize_t siz;
+
+	siz = __procfdreadlink(fd, buf, sizeof(buf));
+	if (siz == -1) {
+		perror("__procfdreadlink");
+		return -1;
+	}
+	buf[siz] = 0;
+	real_path = buf;
+
+	__verbose("%s(fd: %i -> '%s', mode: 0%03o)\n", __func__, fd, real_path,
+		  mode);
 	__fwarn_if_insuffisant_user_mode(fd, mode);
 
 	return next_fchmod(fd, mode);

fchown.c
@@ -37,11 +37,23 @@ int next_fchown(int fd, uid_t owner, gid_t group)
 
 int fchown(int fd, uid_t owner, gid_t group)
 {
+	char buf[PATH_MAX];
+	char *real_path;
+	ssize_t siz;
+
+	siz = __procfdreadlink(fd, buf, sizeof(buf));
+	if (siz == -1) {
+		perror("__procfdreadlink");
+		return -1;
+	}
+	buf[siz] = 0;
+	real_path = buf;
+
 	owner = next_geteuid();
 	group = getegid();
 
-	__verbose("%s(fd: %i, owner: %i, group: %i)\n", __func__, fd, owner,
-		  group);
+	__verbose("%s(fd: %i -> '%s', owner: %i, group: %i)\n", __func__, fd,
+		  real_path, owner, group);
 
 	return next_fchown(fd, owner, group);
 }

fstat64.c
@@ -47,7 +47,19 @@ int next_fstat64(int fd, struct stat64 *statbuf)
 
 int fstat64(int fd, struct stat64 *statbuf)
 {
-	__verbose("%s(fd: %i, ...)\n", __func__, fd);
+	char buf[PATH_MAX];
+	char *real_path;
+	ssize_t siz;
+
+	siz = __procfdreadlink(fd, buf, sizeof(buf));
+	if (siz == -1) {
+		perror("__procfdreadlink");
+		return -1;
+	}
+	buf[siz] = 0;
+	real_path = buf;
+
+	__verbose("%s(fd: %i <-> '%s', ...)\n", __func__, fd, real_path);
 
 	return rootfstat64(fd, statbuf);
 }

iamroot.h
@@ -43,6 +43,7 @@ char *path_resolution(const char *, char *, size_t, int);
 char *fpath_resolutionat(int, const char *, char *, size_t, int);
 
 void __procfdname(char *, unsigned);
+ssize_t __procfdreadlink(int, char *, size_t);
 
 const char *getrootdir();
 int chrootdir(const char *);

commit bef81eda397e26bf049ed06a3dbcca852f8e780c

fpath_resolutionat: remove unused header
The header assert was introduced by mistake with the commit
0b9374de24d5d1a5fd67e533c3760978cb7d319b.

fpath_resolutionat.c
@@ -13,7 +13,7 @@
 #include <dlfcn.h>
 #include <sys/stat.h>
 #include <regex.h>
-#include <assert.h>
+
 #include <fcntl.h>
 
 #include "iamroot.h"
29a7611d23d85c2618819d639ea717d8fdea7ab5
Gaël PORTAY <gael.portay@collabora.com>
2022-01-16 22:02:10 +0100

    {,__x}mknod: forward to {,__x}mknodat()

--- mknod.c
+++ mknod.c
@@ -4,40 +4,16 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later
  */
 
-#include <unistd.h>
 #include <stdio.h>
-#include <errno.h>
-#include <limits.h>
 #include <fcntl.h>
 
 #include <sys/stat.h>
 
 #include "iamroot.h"
 
-extern int next_creat(const char *, mode_t);
-
 int mknod(const char *path, mode_t mode, dev_t dev)
 {
-	char buf[PATH_MAX];
-	char *real_path;
-	int fd;
-	(void)dev;
+	__debug("%s(path: '%s', mode: 0%03o)\n", __func__, path, mode);
 
-	real_path = path_resolution(AT_FDCWD, path, buf, sizeof(buf), 0);
-	if (!real_path) {
-		__pathperror(path, "path_resolution");
-		return -1;
-	}
-
-	__debug("%s(path: '%s' -> '%s', mode: 0%03o)\n", __func__, path,
-		real_path, mode);
-	__warn_if_insuffisant_user_mode(real_path, mode);
-
-	fd = next_creat(path, mode);
-	if (fd == -1)
-		return -1;
-
-	__close(fd);
-
-	return 0;
+	return mknodat(AT_FDCWD, path, mode, dev);
 }

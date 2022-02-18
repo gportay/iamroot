gportay/iamroot

commit eea0360807a960bb3ef987c38cd9cf9089ce9a9e

{,f}ch{mod,own}{,at}: ignore EPERM error

path_resolution.c
@@ -155,6 +155,14 @@ char *sanitize(char *path, size_t bufsize)
 	return path;
 }
 
+int path_ignored(int fd, const char *path, int flags)
+{
+	if (fd != AT_FDCWD)
+		return 0;
+
+	return ignore(path) || (flags & AT_EMPTY_PATH) != 0;
+}
+
 char *path_resolution(int fd, const char *path, char *buf, size_t bufsize,
 		      int flags)
 {

chmod.c
@@ -38,6 +38,7 @@ int chmod(const char *path, mode_t mode)
 {
 	char buf[PATH_MAX];
 	char *real_path;
+	int ret;
 
 	real_path = path_resolution(AT_FDCWD, path, buf, sizeof(buf), 0);
 	if (!real_path) {
@@ -49,5 +50,8 @@ int chmod(const char *path, mode_t mode)
 		real_path, mode);
 	__warn_if_insuffisant_user_mode(real_path, mode);
 
-	return next_chmod(real_path, mode);
+	ret = next_chmod(real_path, mode);
+	__ignore_error_and_warn(ret, AT_FDCWD, path, 0);
+
+	return ret;
 }

chown.c
@@ -40,6 +40,7 @@ int chown(const char *path, uid_t owner, gid_t group)
 {
 	char buf[PATH_MAX];
 	char *real_path;
+	int ret;
 
 	real_path = path_resolution(AT_FDCWD, path, buf, sizeof(buf), 0);
 	if (!real_path) {
@@ -53,5 +54,8 @@ int chown(const char *path, uid_t owner, gid_t group)
 	__debug("%s(path: '%s' -> '%s', owner: %i, group: %i)\n", __func__,
 		path, real_path, owner, group);
 
-	return next_chown(real_path, owner, group);
+	ret = next_chown(real_path, owner, group);
+	__ignore_error_and_warn(ret, AT_FDCWD, path, 0);
+
+	return ret;
 }

fchmod.c
@@ -1,5 +1,5 @@
 /*
- * Copyright 2021 Gaël PORTAY
+ * Copyright 2021-2022 Gaël PORTAY
  *
  * SPDX-License-Identifier: LGPL-2.1-or-later
  */
@@ -7,6 +7,7 @@
 #include <stdio.h>
 #include <errno.h>
 #include <limits.h>
+#include <fcntl.h>
 #include <dlfcn.h>
 
 #include <sys/stat.h>
@@ -38,6 +39,7 @@ int fchmod(int fd, mode_t mode)
 	char buf[PATH_MAX];
 	char *real_path;
 	ssize_t siz;
+	int ret;
 
 	siz = __procfdreadlink(fd, buf, sizeof(buf));
 	if (siz == -1) {
@@ -51,5 +53,8 @@ int fchmod(int fd, mode_t mode)
 		mode);
 	__fwarn_if_insuffisant_user_mode(fd, mode);
 
-	return next_fchmod(fd, mode);
+	ret = next_fchmod(fd, mode);
+	__ignore_error_and_warn(ret, AT_FDCWD, real_path, 0);
+
+	return ret;
 }

fchmodat.c
@@ -38,6 +38,7 @@ int fchmodat(int fd, const char *path, mode_t mode, int flags)
 {
 	char buf[PATH_MAX];
 	char *real_path;
+	int ret;
 
 	real_path = path_resolution(fd, path, buf, sizeof(buf), flags);
 	if (!real_path) {
@@ -49,5 +50,8 @@ int fchmodat(int fd, const char *path, mode_t mode, int flags)
 		path, real_path, mode);
 	__fwarn_if_insuffisant_user_modeat(fd, real_path, mode, flags);
 
-	return next_fchmodat(fd, real_path, mode, flags);
+	ret = next_fchmodat(fd, real_path, mode, flags);
+	__ignore_error_and_warn(ret, fd, path, flags);
+
+	return ret;
 }

fchown.c
@@ -1,5 +1,5 @@
 /*
- * Copyright 2021 Gaël PORTAY
+ * Copyright 2021-2022 Gaël PORTAY
  *
  * SPDX-License-Identifier: LGPL-2.1-or-later
  */
@@ -7,6 +7,7 @@
 #include <stdio.h>
 #include <errno.h>
 #include <limits.h>
+#include <fcntl.h>
 #include <dlfcn.h>
 
 #include <unistd.h>
@@ -40,6 +41,7 @@ int fchown(int fd, uid_t owner, gid_t group)
 	char buf[PATH_MAX];
 	char *real_path;
 	ssize_t siz;
+	int ret;
 
 	siz = __procfdreadlink(fd, buf, sizeof(buf));
 	if (siz == -1) {
@@ -55,5 +57,8 @@ int fchown(int fd, uid_t owner, gid_t group)
 	__debug("%s(fd: %i <-> '%s', owner: %i, group: %i)\n", __func__, fd,
 		real_path, owner, group);
 
-	return next_fchown(fd, owner, group);
+	ret = next_fchown(fd, owner, group);
+	__ignore_error_and_warn(ret, AT_FDCWD, real_path, 0);
+
+	return ret;
 }

fchownat.c
@@ -40,6 +40,7 @@ int fchownat(int fd, const char *path, uid_t owner, gid_t group, int flags)
 {
 	char buf[PATH_MAX];
 	char *real_path;
+	int ret;
 
 	real_path = path_resolution(fd, path, buf, sizeof(buf), flags);
 	if (!real_path) {
@@ -53,5 +54,8 @@ int fchownat(int fd, const char *path, uid_t owner, gid_t group, int flags)
 	__debug("%s(fd: %i, path: '%s' -> '%s', owner: %i, group: %i)\n",
 		__func__, fd, path, real_path, owner, group);
 
-	return next_fchownat(fd, real_path, owner, group, flags);
+	ret = next_fchownat(fd, real_path, owner, group, flags);
+	__ignore_error_and_warn(ret, fd, path, flags);
+
+	return ret;
 }

iamroot.h
@@ -83,6 +83,7 @@ int pathprependenv(const char *, const char *, int);
 int pathsetenv(const char *, const char *, const char *, int);
 
 char *sanitize(char *, size_t);
+int path_ignored(int, const char *, int);
 char *path_resolution(int, const char *, char *, size_t, int);
 
 void __procfdname(char *, unsigned);
@@ -160,6 +161,15 @@ int __verbosef(int, const char *, const char *, ...) __attribute__((format(print
 
 #define __warn_if_too_restrictive_umask(mask) \
 	({ __warn_and_set_umask(mask, 0400); })
+
+#define __ignored_error(rc) ((rc == -1) && (errno == EPERM))
+
+#define __ignore_error_and_warn(rc, fd, path, flags) \
+	({ if (__ignored_error(rc) && (path_ignored(fd, path, flags) > 0)) { \
+	     __warning("%s: %d/%s: Ignoring error '%m'!\n", __func__, fd, path); \
+	     rc = 0; \
+	     errno = 0; \
+	   } })
 
 extern void __pathperror(const char *, const char *);
 extern void __pathperror2(const char *, const char *, const char *);

commit b55ce3a2e1fe0a94a10da2f9d65a787c4b67ea06

path_resolution: handle ignored paths in path_ignored()
This continue the work of ignoring EPERM errors started by the commit
eea0360807a960bb3ef987c38cd9cf9089ce9a9e.

Fixes:

	$ iamroot-shell -c "pacstrap -GMC support/x86_64-pacman.conf arch-rootfs"
	(...)
	( 6/10) Creating temporary files...
	fchownat() of /run/systemd/netif failed: Operation not permitted
	fchownat() of /run/systemd/netif/links failed: Operation not permitted
	fchownat() of /run/systemd/netif/leases failed: Operation not permitted
	fchownat() of /run/systemd/netif/lldp failed: Operation not permitted
	fchownat() of /dev/snd/seq failed: Operation not permitted
	fchownat() of /dev/snd/timer failed: Operation not permitted
	fchownat() of /dev/loop-control failed: Operation not permitted
	Warning: chmod: /dev/vhost-net: Ignoring error 'Operation not permitted'!
	fchownat() of /run/log/journal failed: Operation not permitted
	error: command failed to execute correctly

path_resolution.c
@@ -162,10 +162,30 @@ char *sanitize(char *path, size_t bufsize)
 	return path;
 }
 
+char *fpath(int fd, char *buf, size_t bufsiz)
+{
+	ssize_t siz;
+
+	siz = __procfdreadlink(fd, buf, bufsiz);
+	if (siz == -1)
+		return NULL;
+	buf[siz] = 0; /* ensure NULL terminated */
+
+	return buf;
+}
+
 int path_ignored(int fd, const char *path, int flags)
 {
-	if (fd != AT_FDCWD)
-		return 0;
+	if (fd != AT_FDCWD) {
+		char buf[PATH_MAX];
+		char *real_path;
+
+		real_path = fpath(fd, buf, sizeof(buf));
+		if (!real_path)
+			return -1;
+
+		return ignore(real_path);
+	}
 
 	return ignore(path) || (flags & AT_EMPTY_PATH) != 0;
 }
@@ -207,23 +227,23 @@ char *path_resolution(int fd, const char *path, char *buf, size_t bufsize,
 			return NULL;
 		}
 	} else if (fd != AT_FDCWD) {
-		char dir[PATH_MAX];
-		ssize_t siz;
+		char dirbuf[PATH_MAX];
+		char *real_dir;
 		int size;
 
-		siz = __procfdreadlink(fd, dir, sizeof(dir));
-		if (siz == -1) {
-			__fpathperror(fd, "__procfdreadlink");
+		real_dir = fpath(fd, dirbuf, sizeof(dirbuf));
+		if (!real_dir) {
+			__fpathperror(fd, "fpath");
 			return NULL;
 		}
-		dir[siz] = 0; /* ensure NULL terminated */
 
-		if (*dir != '/') {
-			__warning("%s: ignore '/proc/self/fd/%d'\n", dir, fd);
+		if (*real_dir != '/') {
+			__warning("%s: ignore '/proc/self/fd/%d'\n", real_dir,
+				  fd);
 			goto ignore;
 		}
 
-		size = snprintf(buf, bufsize, "%s/%s", dir, path);
+		size = snprintf(buf, bufsize, "%s/%s", real_dir, path);
 		if (size < 0) {
 			errno = EINVAL;
 			return NULL;
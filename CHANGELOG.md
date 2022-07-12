53a0b93b268a523cc326c4d8f6d3f1e11f6a72ca
Gaël PORTAY <gael.portay@gmail.com>
2022-07-12 10:34:17 -0400

    utimes: forward to utimensat()
    utimes: musl calls indirectly utimensat(AT_FDCWD, ..., 0) via
    __futimesat()
     - https://git.musl-libc.org/cgit/musl/tree/src/linux/utimes.c?h=v1.2.3#n7
    
    __futimesat: calls utimensat(..., 0)
     - https://git.musl-libc.org/cgit/musl/tree/src/stat/futimesat.c?h=v1.2.3#n19
    
    utimensat: calls utimensat(), or futimesat(), or utimes()
     - https://git.musl-libc.org/cgit/musl/tree/src/stat/utimensat.c?h=v1.2.3#n35
     - https://git.musl-libc.org/cgit/musl/tree/src/stat/utimensat.c?h=v1.2.3#n55
     - https://git.musl-libc.org/cgit/musl/tree/src/stat/utimensat.c?h=v1.2.3#n57
    
    utimes: glibc calls via utimensat(AT_FDCWD, ..., 0) via __utimes64()
     - https://sourceware.org/git/?p=glibc.git;a=blob;f=sysdeps/unix/sysv/linux/utimes.c;hb=glibc-2.35#l50
    
    __utimes64: calls __utimensat64_helper(AT_FDCWD, ..., 0)
     - https://sourceware.org/git/?p=glibc.git;a=blob;f=sysdeps/unix/sysv/linux/utimes.c;hb=glibc-2.35#l33
    
    __utimensat64_helper: calls utimensat_time64() or utimensat()
     - https://sourceware.org/git/?p=glibc.git;a=blob;f=sysdeps/unix/sysv/linux/utimensat.c;hb=glibc-2.35#l49
     - https://sourceware.org/git/?p=glibc.git;a=blob;f=sysdeps/unix/sysv/linux/utimensat.c;hb=glibc-2.35#l65

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -15,6 +15,7 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/).
   `mkdir()`, `mkfifo()`, `readlink()`, `rename()`, `symlink()`, and `unlink()`
   to the `at` functions `faccessat()`, `linkat()`, `mkdirat()`, `mkfifoat()`,
   `readlinkat()`, `renameat()`, `symlinkat()`, and `unlinkat()`
+- Forward the none-`at` function `utimes()` to the `at` function `utimensat()`
 
 ### Fixed
 
--- utimes.c
+++ utimes.c
@@ -5,48 +5,15 @@
  */
 
 #include <stdio.h>
-#include <errno.h>
-#include <limits.h>
 #include <fcntl.h>
-#include <dlfcn.h>
 
 #include <sys/time.h>
 
 #include "iamroot.h"
 
-__attribute__((visibility("hidden")))
-int next_utimes(const char *path, const struct timeval times[2])
-{
-	int (*sym)(const char *, const struct timeval[2]);
-	int ret;
-
-	sym = dlsym(RTLD_NEXT, "utimes");
-	if (!sym) {
-		__dlperror(__func__);
-		errno = ENOSYS;
-		return -1;
-	}
-
-	ret = sym(path, times);
-	if (ret == -1)
-		__pathperror(path, __func__);
-
-	return ret;
-}
-
 int utimes(const char *path, const struct timeval times[2])
 {
-	char buf[PATH_MAX];
-	ssize_t siz;
+	__debug("%s(path: '%s', ...)\n", __func__, path);
 
-	siz = path_resolution(AT_FDCWD, path, buf, sizeof(buf),
-			      AT_SYMLINK_NOFOLLOW);
-	if (siz == -1) {
-		__pathperror(path, __func__);
-		return -1;
-	}
-
-	__debug("%s(path: '%s' -> '%s', ...)\n", __func__, path, buf);
-
-	return next_utimes(buf, times);
+	return futimesat(AT_FDCWD, path, times);
 }

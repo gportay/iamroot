6f42ab8c7ac0d98ee2caaa82ace3c7079ee77f80
Gaël PORTAY <gael.portay@gmail.com>
2022-07-19 11:42:27 -0400

    freebsd.mk: set and export IAMROOT_PATH
    Set IAMROOT_PATH to _PATH_DEFPATH[1].
    
    [1]: https://github.com/freebsd/freebsd-src/blob/34b38e1245edc730c4a5707cac2121780031770b/include/paths.h#L42-L44

--- support/freebsd.mk
+++ support/freebsd.mk
@@ -13,6 +13,9 @@ export IAMROOT_LIB_ELF_1
 IAMROOT_PATH_RESOLUTION_IGNORE = ^/(dev|var/run)/|^/etc/resolv.conf
 export IAMROOT_PATH_RESOLUTION_IGNORE
 
+IAMROOT_PATH = /sbin:/bin:/usr/sbin:/usr/bin:/usr/local/sbin:/usr/local/bin
+export IAMROOT_PATH
+
 -include local.mk
 
 .PHONY: all

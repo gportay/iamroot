911aa50048c5d975434362cef969daab93376d0d
Gaël PORTAY <gael.portay@gmail.com>
2022-08-18 15:44:30 -0400

    tests: add mkfifo{,at}

---
+++ tests/mkfifo.c
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2022 Gaël PORTAY
+ *
+ * SPDX-License-Identifier: LGPL-2.1-or-later
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+
+int main(int argc, char * const argv[])
+{
+	mode_t mode;
+
+	if (argc < 3) {
+		fprintf(stderr, "Too few arguments\n");
+		exit(EXIT_FAILURE);
+	} else if (argc > 3) {
+		fprintf(stderr, "Too many arguments\n");
+		exit(EXIT_FAILURE);
+	}
+
+	mode = strtoul(argv[2], NULL, 0);
+
+	if (mkfifo(argv[1], mode)) {
+		perror("mkfifo");
+		return EXIT_FAILURE;
+	}
+
+	return EXIT_SUCCESS;
+}
---
+++ tests/mkfifoat.c
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2022 Gaël PORTAY
+ *
+ * SPDX-License-Identifier: LGPL-2.1-or-later
+ */
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
+
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+
+#include "iamroot.h"
+
+int main(int argc, char * const argv[])
+{
+	int fd = AT_FDCWD, ret = EXIT_FAILURE;
+	mode_t mode;
+
+	if (argc < 4) {
+		fprintf(stderr, "Too few arguments\n");
+		exit(EXIT_FAILURE);
+	} else if (argc > 4) {
+		fprintf(stderr, "Too many arguments\n");
+		exit(EXIT_FAILURE);
+	}
+
+	mode = strtoul(argv[3], NULL, 0);
+
+	if (__strncmp(argv[1], "-") != 0) {
+		fd = open(".", O_DIRECTORY);
+		if (fd == -1) {
+			perror("open");
+			return EXIT_FAILURE;
+		}
+	}
+
+	if (mkfifoat(fd, argv[2], mode)) {
+		perror("mkfifoat");
+		goto exit;
+	}
+
+	ret = EXIT_SUCCESS;
+
+exit:
+	if (fd != AT_FDCWD)
+		if (close(fd))
+			perror("close");
+
+	return ret;
+}

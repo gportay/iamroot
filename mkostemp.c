cdae238c5cc5c98c501ca83020dba5b88ead9b6b
Gaël PORTAY <gael.portay@gmail.com>
2022-08-19 05:40:11 -0400

    mkostemp: forward to mkostemps()
    mkostemp: musl calls __mkostemps(..., 0, ...) which implements
    mkostemp()
     - https://git.musl-libc.org/cgit/musl/tree/src/temp/mkostemp.c?h=v1.2.3#n6
     - https://git.musl-libc.org/cgit/musl/tree/src/temp/mkostemps.c?h=v1.2.3#n28
    
    mkostemp: glibc calls __open() via __try_file(), try_tempname_len(),
    gen_tempname_len(..., 6) and __gen_tempname(..., 0, ..., __GT_FILE)
     - https://sourceware.org/git/?p=glibc.git;a=blob;f=misc/mkostemp.c;hb=glibc-2.36#l33
     - https://sourceware.org/git/?p=glibc.git;a=blob;f=sysdeps/posix/tempname.c;hb=glibc-2.36#l335
     - https://sourceware.org/git/?p=glibc.git;a=blob;f=sysdeps/posix/tempname.c;hb=glibc-2.36#l238
     - https://sourceware.org/git/?p=glibc.git;a=blob;f=sysdeps/posix/tempname.c;hb=glibc-2.36#l175
     - https://sourceware.org/git/?p=glibc.git;a=blob;f=sysdeps/posix/tempname.c;hb=glibc-2.36#l178

--- mkostemp.c
+++ mkostemp.c
@@ -5,61 +5,17 @@
  */
 
 #include <stdio.h>
-#include <string.h>
 #include <errno.h>
-#include <limits.h>
-#include <fcntl.h>
-#include <dlfcn.h>
 
 #include <stdlib.h>
 
 #include "iamroot.h"
 
-__attribute__((visibility("hidden")))
-int next_mkostemp(char *path, int flags)
-{
-	int (*sym)(char *, int);
-	int ret;
-
-	sym = dlsym(RTLD_NEXT, "mkostemp");
-	if (!sym) {
-		__dlperror(__func__);
-		errno = ENOSYS;
-		return -1;
-	}
-
-	ret = sym(path, flags);
-	if (ret == -1)
-		__pathperror(path, __func__);
-
-	return ret;
-}
-
 int mkostemp(char *path, int flags)
 {
-	char buf[PATH_MAX];
-	ssize_t siz;
-	size_t len;
-	int ret;
+	__debug("%s(path: '%s', flags: 0%o)\n", __func__, path, flags);
 
-	siz = path_resolution(AT_FDCWD, path, buf, sizeof(buf), 0);
-	if (siz == -1) {
-		__pathperror(path, __func__);
-		return -1;
-	}
-
-	ret = next_mkostemp(buf, flags);
-	if (ret == -1)
-		goto exit;
-
-	len = __strlen(path);
-	memcpy(path, buf+__strlen(buf)-len, len);
-
-exit:
-	__debug("%s(path: '%s' -> '%s', flags: 0%o)\n", __func__, path, buf,
-		flags);
-
-	return ret;
+	return mkostemps(path, 0, flags);
 }
 
 #ifdef __GLIBC__

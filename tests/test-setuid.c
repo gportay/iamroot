2142cff0756d8f64844622ea955f8f211e26d606
Gaël PORTAY <gael.portay@gmail.com>
2022-08-23 05:33:26 -0400

    tests: add getuid and setuid

---
+++ tests/test-setuid.c
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2022 Gaël PORTAY
+ *
+ * SPDX-License-Identifier: LGPL-2.1-or-later
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#include <unistd.h>
+
+int main(int argc, char * const argv[])
+{
+	uid_t uid;
+
+	if (argc < 2) {
+		fprintf(stderr, "Too few arguments\n");
+		exit(EXIT_FAILURE);
+	} else if (argc > 2) {
+		fprintf(stderr, "Too many arguments\n");
+		exit(EXIT_FAILURE);
+	}
+
+	uid = strtoul(argv[1], NULL, 0);
+
+	if (setuid(uid) == -1) {
+		perror("setuid");
+		return EXIT_FAILURE;
+	}
+
+	return EXIT_SUCCESS;
+}

3e57b8e7f30523cfc6bdf54fbc2f285b9c9a8ff5
Gaël PORTAY <gael.portay@gmail.com>
2022-08-23 05:22:34 -0400

    tests: add name_to_handle_at

---
+++ tests/test-name_to_handle_at.c
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2022 Gaël PORTAY
+ *
+ * SPDX-License-Identifier: LGPL-2.1-or-later
+ */
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+
+#include <fcntl.h>
+
+#include "iamroot.h"
+
+#ifdef __linux__
+int main(int argc, char * const argv[])
+{
+	int id, atflags = 0, dfd = AT_FDCWD, ret = EXIT_FAILURE;
+	unsigned char buf[MAX_HANDLE_SZ];
+	struct file_handle *h;
+
+	if (argc < 3) {
+		fprintf(stderr, "Too few arguments\n");
+		exit(EXIT_FAILURE);
+	} else if (argc > 5) {
+		fprintf(stderr, "Too many arguments\n");
+		exit(EXIT_FAILURE);
+	}
+
+	atflags = strtoul(argv[3], NULL, 0);
+
+	if (__strncmp(argv[1], "-") != 0) {
+		dfd = open(".", O_DIRECTORY);
+		if (dfd == -1) {
+			perror("open");
+			return EXIT_FAILURE;
+		}
+	}
+
+	h = (struct file_handle *)buf;
+	h->handle_bytes = sizeof(buf);
+	if (name_to_handle_at(dfd, argv[2], h, &id, atflags) == -1) {
+		perror("name_to_handle_at");
+		goto exit;
+	}
+
+	printf("%i %u %i\n", id, h->handle_type, h->handle_bytes);
+
+	ret = EXIT_SUCCESS;
+
+exit:
+	if (dfd != AT_FDCWD)
+		if (close(dfd))
+			perror("close");
+
+	return ret;
+}
+#else
+int main(void)
+{
+	return EXIT_SUCCESS;
+}
+#endif

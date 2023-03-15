75a171c9dfbad408a0b968df1463acede3a3ec3c
Gaël PORTAY <gael.portay@rtone.fr>
2023-03-15 22:41:42 +0100

    path_resolution: add warning if directory fd does not contain root
    See commit 737771268e7c7dc1b1d8a336e1ce3c2d99abd9a2.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -10,6 +10,7 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/).
 
 - Output the directory fd for the functions `name_to_handle_at()` and
   `scandirat()`
+- Output a warning if the directory fd does not contain the root directory
 
 ### Fixed
 
--- path_resolution.c
+++ path_resolution.c
@@ -604,6 +604,10 @@ ssize_t path_resolution(int dfd, const char *path, char *buf, size_t bufsize,
 			goto ignore;
 		}
 
+		if (*root && __strlcmp(dirbuf, root) != 0 && !ignore(dirbuf))
+			__warn_or_fatal("%i <-> '%s': does not contains root directory '%s'\n",
+					dfd, dirbuf, root);
+
 		n = _snprintf(buf, bufsize, "%s/%s", dirbuf, path);
 		if (n < 0)
 			return -1;

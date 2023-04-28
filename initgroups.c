54def79d2a8af56d18dcce58bc65de2a919e0379
Gaël PORTAY <gael.portay@rtone.fr>
2023-04-28 20:01:27 +0200

    Fix "Add initgroups()"
    This fixes commit da438f1c1f4d50b43780043e74dc58970cb1cc2c

--- initgroups.c
+++ initgroups.c
@@ -15,10 +15,10 @@
 #include <grp.h>
 #include <limits.h>
 
-int initgroups(const char *user, gid_t gid)                                     
-{                                                                               
-        gid_t groups[NGROUPS_MAX];                                              
-        int count = NGROUPS_MAX;                                                
-        if (getgrouplist(user, gid, groups, &count) < 0) return -1;             
-        return setgroups(count, groups);                                        
+int initgroups(const char *user, gid_t gid)
+{
+        gid_t groups[NGROUPS_MAX];
+        int count = NGROUPS_MAX;
+        if (getgrouplist(user, gid, groups, &count) < 0) return -1;
+        return setgroups(count, groups);
 }

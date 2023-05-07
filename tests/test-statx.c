5226c46cc25ace753b4e56d396d9c79bf1b2c545
Gaël PORTAY <gael.portay@rtone.fr>
2023-05-08 01:52:18 +0200

    Fix "Add statx()"
    This fixes commit 0104b189083c30e392ad6d1f473efdd683df7b6c.

--- tests/test-statx.c
+++ tests/test-statx.c
@@ -30,13 +30,13 @@ int main(int argc, char * const argv[])
 	if (argc < 5) {
 		fprintf(stderr, "Too few arguments\n");
 		exit(EXIT_FAILURE);
-	} else if (argc > 6) {
+	} else if (argc > 5) {
 		fprintf(stderr, "Too many arguments\n");
 		exit(EXIT_FAILURE);
 	}
 
 	flags = strtoul(argv[3], NULL, 0);
-	mask = strtoul(argv[3], NULL, 0);
+	mask = strtoul(argv[4], NULL, 0);
 
 	if (!__strneq(argv[1], "-")) {
 		dfd = open(".", O_RDONLY);

5c01b6de461f45155d35c465e8dac43a029a71c5
Gaël PORTAY <gael.portay@rtone.fr>
2023-05-13 00:14:25 +0200

    Fix "Import group familly functions from musl"
    This fixes commit 214d6064c6a4c25c2d1812223f17328a981cb321.

--- group.c
+++ group.c
@@ -1,5 +1,5 @@
 /*
- * Copyright 2021-2022 Gaël PORTAY
+ * Copyright 2021-2023 Gaël PORTAY
  *
  * SPDX-License-Identifier: LGPL-2.1-or-later
  */
@@ -26,7 +26,6 @@
  */
 #include <pwd.h>
 #include <grp.h>
-#include <shadow.h>
 #include <stdio.h>
 #include <errno.h>
 #include <stdint.h>
@@ -38,7 +37,6 @@ hidden int __getpwent_a(FILE *f, struct passwd *pw, char **line, size_t *size, s
 hidden int __getpw_a(const char *name, uid_t uid, struct passwd *pw, char **buf, size_t *size, struct passwd **res);
 hidden int __getgrent_a(FILE *f, struct group *gr, char **line, size_t *size, char ***mem, size_t *nmem, struct group **res);
 hidden int __getgr_a(const char *name, gid_t gid, struct group *gr, char **buf, size_t *size, char ***mem, size_t *nmem, struct group **res);
-hidden int __parsespent(char *s, struct spwd *sp);
 
 /*
  * Stolen and hacked from musl (src/passwd/getgrent_a.c)

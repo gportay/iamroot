b589917ff5f5bab457444b51388f6e5a5d95a959
Gaël PORTAY <gael.portay@rtone.fr>
2023-07-03 15:32:25 +0200

    Add comments about interpreter scripts from man execve(2)

--- execve.c
+++ execve.c
@@ -222,6 +222,39 @@ static ssize_t __gethashbang(const char *path, char *buf, size_t bufsize)
 	char *d, *s;
 	int fd;
 
+	/*
+	 * According to man execve(2):
+	 *
+	 * Interpreter scripts
+	 *
+	 * An interpreter script is a text file that has execute permission
+	 * enabled and whose first line is of the form:
+	 *
+	 * #!interpreter [optional-arg]
+	 *
+	 * The interpreter must be a valid pathname for an executable file.
+	 *
+	 * If the pathname argument of execve() specifies an interpreter
+	 * script, then interpreter will be invoked with the following
+	 * arguments:
+	 *
+	 * interpreter [optional-arg] pathname arg...
+	 *
+	 * where pathname is the pathname of the file specified as the first
+	 * argument of execve(), and arg... is the series of words pointed to
+	 * by the argv argument of execve(), starting at argv[1]. Note that
+	 * there is no way to get the argv[0] that was passed to the execve()
+	 * call.
+	 *
+	 * For portable use, optional-arg should either be absent, or be
+	 * specified as a single word (i.e., it should not contain white
+	 * space); see NOTES below.
+	 *
+	 * Since Linux 2.6.28, the kernel permits the interpreter of a script
+	 * to itself be a script. This permission is recursive, up to a limit
+	 * of four recursions, so that the interpreter may be a script which is
+	 * interpreted by a script, and so on.
+	 */
 	fd = next_open(path, O_RDONLY, 0);
 	if (fd == -1)
 		return -1;
@@ -249,6 +282,20 @@ static ssize_t __gethashbang(const char *path, char *buf, size_t bufsize)
 		*d++ = *s++;
 	*d++ = 0;
 
+	/*
+	 * According to man execve(2):
+	 *
+	 * Interpreter scripts
+	 *
+	 * The semantics of the optional-arg argument of an interpreter script
+	 * vary across implementations. On Linux, the entire string following
+	 * the interpreter name is passed as a single argument to the
+	 * interpreter, and this string can include white space. However,
+	 * behavior differs on some other systems. Some systems use the first
+	 * white space to terminate optional-arg. On some systems, an
+	 * interpreter script can have multiple arguments, and white spaces in
+	 * optional-arg are used to delimit the arguments.
+	 */
 	/* has an optional argument */
 	if (isblank(*s)) {
 		/* skip leading blanks */

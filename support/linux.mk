4ed00faef5db69b30b8a1099c03f52d5b32db145
Gaël PORTAY <gael.portay@rtone.fr>
2023-09-27 07:57:25 +0200

    Revert "linux.mk: remove ubuntu-impish targets"
    Ubuntu Impish Indri was moved to the ubuntu old-releases[1].
    
    This reverts commit 4544e2983f72d18c567d7610838d51a8f25f9b56.
    
    [1]: http://old-releases.ubuntu.com/ubuntu/dists/

--- support/linux.mk
+++ support/linux.mk
@@ -691,6 +691,7 @@ x86_64-ubuntu-rootfs: x86_64-ubuntu-trusty-rootfs
 x86_64-ubuntu-rootfs: x86_64-ubuntu-xenial-rootfs
 x86_64-ubuntu-rootfs: x86_64-ubuntu-bionic-rootfs
 x86_64-ubuntu-rootfs: x86_64-ubuntu-focal-rootfs
+x86_64-ubuntu-rootfs: x86_64-ubuntu-impish-rootfs
 x86_64-ubuntu-rootfs: x86_64-ubuntu-jammy-rootfs
 x86_64-ubuntu-rootfs: x86_64-ubuntu-kinetic-rootfs
 x86_64-ubuntu-rootfs: x86_64-ubuntu-lunar-rootfs
@@ -700,6 +701,7 @@ x86_64-ubuntu-trusty-rootfs/bin/sh: export DEBOOTSTRAP_MIRROR ?= http://archive.
 x86_64-ubuntu-xenial-rootfs/bin/sh: export DEBOOTSTRAP_MIRROR ?= http://archive.ubuntu.com/ubuntu
 x86_64-ubuntu-bionic-rootfs/bin/sh: export DEBOOTSTRAP_MIRROR ?= http://archive.ubuntu.com/ubuntu
 x86_64-ubuntu-focal-rootfs/bin/sh: export DEBOOTSTRAP_MIRROR ?= http://archive.ubuntu.com/ubuntu
+x86_64-ubuntu-impish-rootfs/bin/sh: export DEBOOTSTRAP_MIRROR ?= http://old-releases.ubuntu.com/ubuntu
 x86_64-ubuntu-jammy-rootfs/bin/sh: export DEBOOTSTRAP_MIRROR ?= http://archive.ubuntu.com/ubuntu
 x86_64-ubuntu-kinetic-rootfs/bin/sh: export DEBOOTSTRAP_MIRROR ?= http://old-releases.ubuntu.com/ubuntu
 x86_64-ubuntu-lunar-rootfs/bin/sh: export DEBOOTSTRAP_MIRROR ?= http://archive.ubuntu.com/ubuntu
@@ -709,6 +711,7 @@ $(eval $(call debootstrap-rootfs,x86_64,ubuntu,trusty))
 $(eval $(call debootstrap-rootfs,x86_64,ubuntu,xenial))
 $(eval $(call debootstrap-rootfs,x86_64,ubuntu,bionic))
 $(eval $(call debootstrap-rootfs,x86_64,ubuntu,focal))
+$(eval $(call debootstrap-rootfs,x86_64,ubuntu,impish))
 $(eval $(call debootstrap-rootfs,x86_64,ubuntu,jammy))
 $(eval $(call debootstrap-rootfs,x86_64,ubuntu,kinetic))
 $(eval $(call debootstrap-rootfs,x86_64,ubuntu,lunar))
@@ -737,6 +740,8 @@ x86_64-ubuntu-bionic-rootfs/bin/sh: export ISH_PRESERVE_ENV := $(ISH_PRESERVE_EN
 x86_64-ubuntu-bionic-rootfs/bin/sh: export LDCONFIG_NOTRIGGER = y
 x86_64-ubuntu-focal-rootfs/bin/sh: export ISH_PRESERVE_ENV := $(ISH_PRESERVE_ENV):LDCONFIG_NOTRIGGER
 x86_64-ubuntu-focal-rootfs/bin/sh: export LDCONFIG_NOTRIGGER = y
+x86_64-ubuntu-impish-rootfs/bin/sh: export ISH_PRESERVE_ENV := $(ISH_PRESERVE_ENV):LDCONFIG_NOTRIGGER
+x86_64-ubuntu-impish-rootfs/bin/sh: export LDCONFIG_NOTRIGGER = y
 x86_64-ubuntu-jammy-rootfs/bin/sh: export ISH_PRESERVE_ENV := $(ISH_PRESERVE_ENV):LDCONFIG_NOTRIGGER
 x86_64-ubuntu-jammy-rootfs/bin/sh: export LDCONFIG_NOTRIGGER = y
 x86_64-ubuntu-kinetic-rootfs/bin/sh: export ISH_PRESERVE_ENV := $(ISH_PRESERVE_ENV):LDCONFIG_NOTRIGGER
@@ -1219,6 +1224,7 @@ ubuntu-support: support/x86_64-ubuntu-trusty-rootfs.txt
 ubuntu-support: support/x86_64-ubuntu-xenial-rootfs.txt
 ubuntu-support: support/x86_64-ubuntu-bionic-rootfs.txt
 ubuntu-support: support/x86_64-ubuntu-focal-rootfs.txt
+ubuntu-support: support/x86_64-ubuntu-impish-rootfs.txt
 ubuntu-support: support/x86_64-ubuntu-jammy-rootfs.txt
 ubuntu-support: support/x86_64-ubuntu-kinetic-rootfs.txt
 ubuntu-support: support/x86_64-ubuntu-lunar-rootfs.txt
@@ -1231,6 +1237,7 @@ ubuntu-log: x86_64-ubuntu-trusty-rootfs.log
 ubuntu-log: x86_64-ubuntu-xenial-rootfs.log
 ubuntu-log: x86_64-ubuntu-bionic-rootfs.log
 ubuntu-log: x86_64-ubuntu-focal-rootfs.log
+ubuntu-log: x86_64-ubuntu-impish-rootfs.log
 ubuntu-log: x86_64-ubuntu-jammy-rootfs.log
 ubuntu-log: x86_64-ubuntu-kinetic-rootfs.log
 ubuntu-log: x86_64-ubuntu-lunar-rootfs.log

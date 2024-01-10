860c666ae353703206b42e864ef9a3f98b3c9bcb
Gaël PORTAY <gael.portay@rtone.fr>
2024-01-10 12:13:38 +0100

    Revert "{,euid}access: forward to faccessat()"
    This reverts commit bcfaf290dd4a162e3c00beab48f469e0faf7b056.

--- euidaccess.c
+++ euidaccess.c
@@ -1,23 +1,50 @@
 /*
- * Copyright 2021-2023 Gaël PORTAY
+ * Copyright 2021-2024 Gaël PORTAY
  *
  * SPDX-License-Identifier: LGPL-2.1-or-later
  */
 
 #include <stdio.h>
 #include <errno.h>
+#include <limits.h>
 #include <fcntl.h>
+#include <dlfcn.h>
 
 #include <unistd.h>
 
 #include "iamroot.h"
 
+static int (*sym)(const char *, int);
+
+__attribute__((visibility("hidden")))
+int next_euidaccess(const char *path, int mode)
+{
+	if (!sym)
+		sym = dlsym(RTLD_NEXT, "euidaccess");
+
+	if (!sym)
+		return __dl_set_errno_and_perror(ENOSYS, -1);
+
+	return sym(path, mode);
+}
+
 int euidaccess(const char *path, int mode)
 {
-	__debug("%s(path: '%s', mode: 0%03o)\n", __func__, path, mode);
+	char buf[PATH_MAX];
+	int ret = -1;
+	ssize_t siz;
 
-	/* Forward to another function */
-	return faccessat(AT_FDCWD, path, mode, AT_EACCESS);
+	siz = path_resolution(AT_FDCWD, path, buf, sizeof(buf), 0);
+	if (siz == -1)
+		goto exit;
+
+	ret = next_euidaccess(buf, mode);
+
+exit:
+	__debug("%s(path: '%s' -> '%s', mode: 0%03o) -> %i\n", __func__, path,
+		buf, mode, ret);
+
+	return ret;
 }
 
 weak_alias(euidaccess, eaccess);

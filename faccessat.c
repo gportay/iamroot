bb7d30e545e75b92741fc4e5f7d06f359b814fc7
Gaël PORTAY <gael.portay@rtone.fr>
2024-01-10 21:01:51 +0100

    faccessat: rename none-AT flags to flags
    According to access(3p):
    
    	Values for flag are constructed by a bitwise-inclusive OR of
    	flags from the following list, defined in <fcntl.h>:
    
    	AT_EACCESS The checks for accessibility (including directory
    		   permissions checked during pathname resolution) shall
    		   be performed using the effective user ID and group ID
    		   instead of the real user ID and group ID as required
    	           in a call to access().
    
    It is unclear if the function faccessat() supports AT-flags other than
    AT_EACCESS[1][2].
    
    Note: The support for AT_SYMLINK_NOFOLLOW is a glibc singularity.
    
    This reverts partially commit 30e60b0b7b78e30994a3130f9b2ff103109d41ce.
    
    See the commit 435a0aa1606b25c2c3da93b3444dc070cacfcecd.
    
    [1]: https://pubs.opengroup.org/onlinepubs/9699919799/functions/access.html
    [2]: https://pubs.opengroup.org/onlinepubs/9699919799/basedefs/fcntl.h.html

--- faccessat.c
+++ faccessat.c
@@ -16,7 +16,7 @@
 
 static int (*sym)(int, const char *, int, int);
 
-hidden int next_faccessat(int dfd, const char *path, int mode, int atflags)
+hidden int next_faccessat(int dfd, const char *path, int mode, int flags)
 {
 	if (!sym)
 		sym = dlsym(RTLD_NEXT, "faccessat");
@@ -24,24 +24,24 @@ hidden int next_faccessat(int dfd, const char *path, int mode, int atflags)
 	if (!sym)
 		return __dl_set_errno_and_perror(ENOSYS, -1);
 
-	return sym(dfd, path, mode, atflags);
+	return sym(dfd, path, mode, flags);
 }
 
-int faccessat(int dfd, const char *path, int mode, int atflags)
+int faccessat(int dfd, const char *path, int mode, int flags)
 {
 	char buf[PATH_MAX];
 	int ret = -1;
 	ssize_t siz;
 
-	siz = path_resolution(dfd, path, buf, sizeof(buf), atflags);
+	siz = path_resolution(dfd, path, buf, sizeof(buf), 0);
 	if (siz == -1)
 		goto exit;
 
-	ret = next_faccessat(dfd, buf, mode, atflags);
+	ret = next_faccessat(dfd, buf, mode, flags);
 
 exit:
-	__debug("%s(dfd: %i <-> '%s', path: '%s' -> '%s', mode: 0%03o, atflags: 0x%x) -> %i\n",
-		__func__, dfd, __fpath(dfd), path, buf, mode, atflags, ret);
+	__debug("%s(dfd: %i <-> '%s', path: '%s' -> '%s', mode: 0%03o, flags: 0x%x) -> %i\n",
+		__func__, dfd, __fpath(dfd), path, buf, mode, flags, ret);
 
 	return ret;
 }

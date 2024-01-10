47d83787a5765f85233a61f3d7b1d0a42f5c23a2
Gaël PORTAY <gael.portay@rtone.fr>
2024-01-10 21:01:51 +0100

    faccessat: remove none-AT flags to flags
    This reverts partially commit 30e60b0b7b78e30994a3130f9b2ff103109d41ce.

--- faccessat.c
+++ faccessat.c
@@ -1,5 +1,5 @@
 /*
- * Copyright 2021-2023 Gaël PORTAY
+ * Copyright 2021-2024 Gaël PORTAY
  *
  * SPDX-License-Identifier: LGPL-2.1-or-later
  */
@@ -17,7 +17,7 @@
 static int (*sym)(int, const char *, int, int);
 
 __attribute__((visibility("hidden")))
-int next_faccessat(int dfd, const char *path, int mode, int atflags)
+int next_faccessat(int dfd, const char *path, int mode, int flags)
 {
 	if (!sym)
 		sym = dlsym(RTLD_NEXT, "faccessat");
@@ -25,11 +25,12 @@ int next_faccessat(int dfd, const char *path, int mode, int atflags)
 	if (!sym)
 		return __dl_set_errno_and_perror(ENOSYS, -1);
 
-	return sym(dfd, path, mode, atflags);
+	return sym(dfd, path, mode, flags);
 }
 
-int faccessat(int dfd, const char *path, int mode, int atflags)
+int faccessat(int dfd, const char *path, int mode, int flags)
 {
+	const int atflags = 0 | flags;
 	char buf[PATH_MAX];
 	int ret = -1;
 	ssize_t siz;
@@ -38,11 +39,11 @@ int faccessat(int dfd, const char *path, int mode, int atflags)
 	if (siz == -1)
 		goto exit;
 
-	ret = next_faccessat(dfd, buf, mode, atflags);
+	ret = next_faccessat(dfd, buf, mode, flags);
 
 exit:
-	__debug("%s(dfd: %i <-> '%s', path: '%s' -> '%s', mode: 0%03o, atflags: 0x%x) -> %i\n",
-		__func__, dfd, __fpath(dfd), path, buf, mode, atflags, ret);
+	__debug("%s(dfd: %i <-> '%s', path: '%s' -> '%s', mode: 0%03o, flags: 0x%x) -> %i\n",
+		__func__, dfd, __fpath(dfd), path, buf, mode, flags, ret);
 
 	return ret;
 }

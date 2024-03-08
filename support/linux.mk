a124bd10e0b58de20544976c8c92d0a317fffaa9
Gaël PORTAY <gael.portay@rtone.fr>
2024-03-09 00:46:18 +0100

    linux.mk: add alpinelinux-{3.14,3.15} to legacy targets

--- support/linux.mk
+++ support/linux.mk
@@ -1824,6 +1824,11 @@ alpinelinux-log: x86_64-alpinelinux-3.18-rootfs.log
 alpinelinux-log: x86_64-alpinelinux-3.19-rootfs.log
 alpinelinux-log: x86_64-alpinelinux-edge-rootfs.log
 
+legacy-support: support/x86_64-alpinelinux-3.14-rootfs.txt
+legacy-support: support/x86_64-alpinelinux-3.15-rootfs.txt
+legacy-log: x86_64-alpinelinux-3.14-rootfs.log
+legacy-log: x86_64-alpinelinux-3.15-rootfs.log
+
 stable-support: support/x86_64-alpinelinux-3.18-rootfs.txt
 stable-log: x86_64-alpinelinux-3.18-rootfs.log
 

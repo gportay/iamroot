92e3fcbc55d89a5028fe203932651ecabe477c18
Gaël PORTAY <gael.portay@rtone.fr>
2024-03-11 04:11:43 +0100

    {net,open}bsd.mk: fix chroot target
    Note: chroot'ing on OpenBSD via chroot(1) is impossible for now as the
    binary is statically linked.
    
    Fixes:
    
    	-bash-5.2$ make netbsd-9.3-chroot
    	bash ido  chroot netbsd-9.3-chroot
    	chroot: netbsd-9.3-chroot: No such file or directory
    	gmake: *** [makefile:56: netbsd-9.3-chroot] Error 1
    
    	-bash-5.2$ make openbsd-73-chroot
    	wget https://cdn.openbsd.org/pub/OpenBSD/7.3/arm64/base73.tgz -O base-73.tgz
    	bash ido  chroot openbsd-73-chroot
    	chroot: openbsd-73-chroot: Operation not permitted
    	gmake: *** [makefile:44: openbsd-73-chroot] Error 1

--- support/netbsd.mk
+++ support/netbsd.mk
@@ -50,7 +50,7 @@ $(O)-%:
 netbsd-9.3-chroot: export PATH = /sbin:/usr/sbin:/bin:/usr/bin:/usr/pkg/sbin:/usr/pkg/bin:/usr/X11R7/bin:/usr/local/sbin:/usr/local/bin
 netbsd-9.3-chroot: export SHELL = /bin/sh
 netbsd-9.3-chroot: $(ARCH)/libiamroot.elf_so | netbsd-9.3-rootfs
-	bash ido $(IDOFLAGS) chroot $@
+	bash ido $(IDOFLAGS) chroot netbsd-9.3-rootfs
 
 ifeq ($(ARCH),x86_64)
 netbsd-9.3-rootfs: | $(ARCH)/libiamroot.elf_so base.tar.xz

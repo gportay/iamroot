b6480fd7f55fddfc9445be26dfc0c135982eb089
Gaël PORTAY <gael.portay@rtone.fr>
2024-03-27 20:10:44 +0100

    {,__x}mknodat: forward to openat()
    The function creat() is implemented on top of openat() (glibc[1],
    musl[2], FreeBSD[3], NetBSD[4] and OpenBSD[5]).
    
    Forwarding the resolved path to next_creat() causes the path to get
    resolved twice.
    
    This forwards the both functions mknodat() and __xmknodat() to the
    function openat() directly.
    
    Fixes:
    
    	-bash-5.2$ make test
    	(...)
    	cd rootfs/ && env LD_PRELOAD=/home/freebox/src/iamroot/aarch64/libiamroot.elf_so:/home/freebox/src/iamroot/tests/rootfs/lib/libc.so.12 PATH=/home/freebox/src/iamroot/tests/rootfs/usr/bin IAMROOT_ROOT=/home/freebox/src/iamroot/tests/rootfs                                                test-mknod                /dev/dev3                0644 0 3
    	Warning: /home/freebox/src/iamroot/tests/rootfs/dev/dev3: contains root directory '/home/freebox/src/iamroot/tests/rootfs'
    	cd rootfs/ && env LD_PRELOAD=/home/freebox/src/iamroot/aarch64/libiamroot.elf_so:/home/freebox/src/iamroot/tests/rootfs/lib/libc.so.12 PATH=/home/freebox/src/iamroot/tests/rootfs/usr/bin IAMROOT_ROOT=/home/freebox/src/iamroot/tests/rootfs                                                test-mknodat            - /dev/dev4                0666 0 4
    	Warning: /home/freebox/src/iamroot/tests/rootfs/dev/dev4: contains root directory '/home/freebox/src/iamroot/tests/rootfs'
    	cd rootfs/ && env LD_PRELOAD=/home/freebox/src/iamroot/aarch64/libiamroot.elf_so:/home/freebox/src/iamroot/tests/rootfs/lib/libc.so.12 PATH=/home/freebox/src/iamroot/tests/rootfs/usr/bin IAMROOT_ROOT=/home/freebox/src/iamroot/tests/rootfs                                                test-mknodat            . /dev/dev5                0666 0 5
    	Warning: /home/freebox/src/iamroot/tests/rootfs/dev/dev5: contains root directory '/home/freebox/src/iamroot/tests/rootfs'
    
    [1]: https://sourceware.org/git/?p=glibc.git;a=blob;f=io/creat.c;hb=refs/tags/glibc-2.39#l28
    [2]: https://git.musl-libc.org/cgit/musl/tree/src/fcntl/creat.c?h=v1.2.5#n5
    [3]: https://github.com/freebsd/freebsd-src/blob/release/14.0.0/lib/libc/compat-43/creat.c#L52
    [4]: https://github.com/netbsd/src/blob/61f282557f0bc41c0b762c629a2f4c14be8b7591/lib/libc/compat-43/creat.c#L48
    [5]: https://github.com/openbsd/src/blob/df930be708d50e9715f173caa26ffe1b7599b157/lib/libc/compat-43/creat.c#L50

--- __xmknodat.c
+++ __xmknodat.c
@@ -5,49 +5,30 @@
  */
 
 #ifdef __linux__
-#include <unistd.h>
 #include <stdio.h>
 #include <errno.h>
-#include <limits.h>
-#include <sys/xattr.h>
 
 #include <fcntl.h>
 #include <sys/stat.h>
 
 #include "iamroot.h"
 
-extern int next_creat(const char *, mode_t);
-
 int __xmknodat(int ver, int dfd, const char *path, mode_t mode, dev_t *dev)
 {
-	const mode_t oldmode = mode;
-	char buf[PATH_MAX];
-	int fd, ret = -1;
-	ssize_t siz;
+	int fd;
 	(void)ver;
-	(void)oldmode;
 	(void)dev;
 
-	siz = path_resolution(dfd, path, buf, sizeof(buf), 0);
-	if (siz == -1)
-		goto exit;
+	__debug("%s(dfd: %i, path: '%s', mode: 0%03o)\n", __func__, dfd, path,
+		mode);
 
-	__warn_if_insuffisant_user_mode(buf, mode);
+	/* Forward to another function */
+	fd = openat(dfd, path, O_CREAT|O_WRONLY|O_TRUNC, mode);
+	if (fd == -1)
+		return -1;
 
-	/* Not forwarding function */
-	ret = next_creat(buf, mode);
-	if (ret == -1)
-		goto exit;
-
-	fd = ret;
-	ret = 0;
 	__close(fd);
-	__set_mode(buf, oldmode, mode);
 
-exit:
-	__debug("%s(dfd: %i <-> '%s', path: '%s' -> '%s', mode: 0%03o -> 0%03o) -> %i\n",
-		__func__, dfd, __fpath(dfd), path, buf, oldmode, mode, ret);
-
-	return ret;
+	return 0;
 }
 #endif

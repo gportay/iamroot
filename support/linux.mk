20f8d749058ef301651da08fc887810d476fae19
Gaël PORTAY <gael.portay@rtone.fr>
2024-05-10 15:34:21 +0200

    linux.mk: add devuan-excalibur targets
    Note: The GPG key devuan-keyring-excalibur-archive.gpg is extracted from
    the archive devuan-keyring_2023.10.07.tar.xz[1].
    
    Note 2: The option --exclude=logrotate,cron,cron-common-daemon intends
    to fix the error below:
    
    	Preparing to unpack .../cron-daemon-common_3.0pl1-189_all.deb ...
    	Unpacking cron-daemon-common (3.0pl1-189) ...
    	Setting up adduser (3.137) ...
    	dpkg: dependency problems prevent configuration of cron-daemon-common:
    	 cron-daemon-common depends on systemd | systemd-standalone-sysusers | systemd-sysusers; however:
    	  Package systemd is not installed.
    	  Package systemd-standalone-sysusers is not installed.
    	  Package systemd-sysusers is not installed.
    
    	dpkg: error processing package cron-daemon-common (--install):
    	 dependency problems - leaving unconfigured
    	Errors were encountered while processing:
    	 cron-daemon-common
    
    And it raises the following error:
    
    	W: Failure while configuring base packages.  This will be re-attempted up to five times.
    	W: See /home/gportay/src/iamroot/amd64-devuan-excalibur-rootfs/debootstrap/debootstrap.log for details (possibly the package cron-daemon-common is at fault)
    	W: Failure while configuring base packages.  This will be re-attempted up to five times.
    	W: See /home/gportay/src/iamroot/amd64-devuan-excalibur-rootfs/debootstrap/debootstrap.log for details (possibly the package cron-daemon-common is at fault)
    	W: Failure while configuring base packages.  This will be re-attempted up to five times.
    	W: See /home/gportay/src/iamroot/amd64-devuan-excalibur-rootfs/debootstrap/debootstrap.log for details (possibly the package cron-daemon-common is at fault)
    	W: Failure while configuring base packages.  This will be re-attempted up to five times.
    	W: See /home/gportay/src/iamroot/amd64-devuan-excalibur-rootfs/debootstrap/debootstrap.log for details (possibly the package cron-daemon-common is at fault)
    	W: Failure while configuring base packages.  This will be re-attempted up to five times.
    	W: See /home/gportay/src/iamroot/amd64-devuan-excalibur-rootfs/debootstrap/debootstrap.log for details (possibly the package cron-daemon-common is at fault)
    
    	$ cat amd64-devuan-excalibur-rootfs/debootstrap/debootstrap.log
    	(...)
    	Errors were encountered while processing:
    	 cron-daemon-common
    	dpkg: dependency problems prevent configuration of cron-daemon-common:
    	 cron-daemon-common depends on systemd | systemd-standalone-sysusers | systemd-sysusers; however:
    	  Package systemd is not installed.
    	  Package systemd-standalone-sysusers is not installed.
    	  Package systemd-sysusers is not installed.
    
    The option --variant=minbase is set to prevent from installing of cron
    as debootstrap installs cron even if it is excluded with the use of the
    option --exclude=logrotate,cron,cron-common-daemon.
    
    For now, both options are kept if the second appears to be unneeded; the
    first one speeds up the build as it installs essencial packages only.
    
    [1]: https://pkgmaster.devuan.org/devuan/pool/main/d/devuan-keyring/devuan-keyring_2023.10.07.tar.xz

--- support/linux.mk
+++ support/linux.mk
@@ -988,11 +988,15 @@ amd64-devuan-chimaera-rootfs/bin/sh: export DEBOOTSTRAP_MIRROR ?= http://deb.dev
 amd64-devuan-chimaera-rootfs/bin/sh: export DEBOOTSTRAP_SCRIPT ?= support/ceres
 amd64-devuan-daedalus-rootfs/bin/sh: export DEBOOTSTRAP_MIRROR ?= http://deb.devuan.org/merged/
 amd64-devuan-daedalus-rootfs/bin/sh: export DEBOOTSTRAP_SCRIPT ?= support/ceres
+amd64-devuan-excalibur-rootfs/bin/sh: export DEBOOTSTRAP_MIRROR ?= http://deb.devuan.org/merged/
+amd64-devuan-excalibur-rootfs/bin/sh: export DEBOOTSTRAP_SCRIPT ?= support/ceres
+amd64-devuan-excalibur-rootfs/bin/sh: export DEBOOTSTRAPFLAGS ?= --keyring support/devuan-keyring-excalibur-archive.gpg --exclude=logrotate,cron,cron-common-daemon --variant=minbase
 $(eval $(call debootstrap-rootfs,amd64,devuan,jessie))
 $(eval $(call debootstrap-rootfs,amd64,devuan,ascii))
 $(eval $(call debootstrap-rootfs,amd64,devuan,beowulf))
 $(eval $(call debootstrap-rootfs,amd64,devuan,chimaera))
 $(eval $(call debootstrap-rootfs,amd64,devuan,daedalus))
+$(eval $(call debootstrap-rootfs,amd64,devuan,excalibur))
 # Processing triggers for libc-bin ...
 # dpkg: cycle found while processing triggers:
 #  chain of packages whose triggers are or may be responsible:
@@ -1017,6 +1021,9 @@ legacy-log: amd64-devuan-chimaera-rootfs.log
 
 stable-support: support/amd64-devuan-daedalus-rootfs.txt
 stable-log: amd64-devuan-daedalus-rootfs.log
+
+unstable-support: support/amd64-devuan-excalibur-rootfs.txt
+unstable-log: amd64-devuan-excalibur-rootfs.log
 
 install-support-x86_64: install-support-x86_64-debootstrap
 
@@ -1970,6 +1977,7 @@ devuan-support: support/amd64-devuan-ascii-rootfs.txt
 devuan-support: support/amd64-devuan-beowulf-rootfs.txt
 devuan-support: support/amd64-devuan-chimaera-rootfs.txt
 devuan-support: support/amd64-devuan-daedalus-rootfs.txt
+devuan-support: support/amd64-devuan-excalibur-rootfs.txt
 
 log: devuan-log
 
@@ -1979,6 +1987,7 @@ devuan-log: amd64-devuan-ascii-rootfs.log
 devuan-log: amd64-devuan-beowulf-rootfs.log
 devuan-log: amd64-devuan-chimaera-rootfs.log
 devuan-log: amd64-devuan-daedalus-rootfs.log
+devuan-log: amd64-devuan-excalibur-rootfs.log
 
 legacy-support: support/amd64-debian-buster-rootfs.txt
 legacy-support: support/amd64-debian-bullseye-rootfs.txt

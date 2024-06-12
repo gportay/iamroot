91c3a9d39eb88a5bac4a4b343cfc1f3dcad52ab5
Gaël PORTAY <gael.portay@rtone.fr>
2024-06-12 03:20:42 +0200

    dup{2,3}: prefer use of ret to __setfd()

--- dup2.c
+++ dup2.c
@@ -33,7 +33,7 @@ int dup2(int oldfd, int newfd)
 	ret = next_dup2(oldfd, newfd);
 
 	if (ret >= 0)
-		__setfd(newfd, __fpath(oldfd));
+		__setfd(ret, __fpath(oldfd));
 
 	__debug("%s(oldfd: %i <-> '%s', newfd: %i <-> '%s') -> %i\n",
 		__func__, oldfd, __fpath(oldfd), newfd, __fpath2(newfd), ret);
--- dup3.c
+++ dup3.c
@@ -33,7 +33,7 @@ int dup3(int oldfd, int newfd, int oflags)
 	ret = next_dup3(oldfd, newfd, oflags);
 
 	if (ret >= 0)
-		__setfd(newfd, __fpath(oldfd));
+		__setfd(ret, __fpath(oldfd));
 
 	__debug("%s(oldfd: %i <-> '%s', newfd: %i <-> '%s', oflags: 0%o) -> %i\n",
 		__func__, oldfd, __fpath(oldfd), newfd, __fpath2(newfd),
